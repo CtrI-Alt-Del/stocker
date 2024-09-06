fabde7a35cdf1182395a89105b920424b59db7ad
0thigs <machinimavlogs@gmail.com>
2024-09-06 10:06:05 -0300

    🔧config: configure tailwind files

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{js,ts,jsx,tsx}',
+    './app/src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
