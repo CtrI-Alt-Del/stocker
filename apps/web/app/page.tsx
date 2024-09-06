e658fe7a32fb7dbe8bb858ac60a0bd7cc4a29572
0thigs <machinimavlogs@gmail.com>
2024-09-05 21:06:34 -0300

    🧱component: remove button Ui turborepo link

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@repo/ui/button";
-
 export default function Home() {
   return (
     <div>
       <h1>Ola Thigszin</h1>
-      <Button appName="Thigszin">Click me</Button>
     </div>
   );
 }
