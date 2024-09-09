5b58d1a5f383e1e45113e4e4e31dbaafd10b9e4f
0thigs <machinimavlogs@gmail.com>
2024-09-09 17:21:02 -0300

    🧱component: add providers to layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
+
 
 export const metadata: Metadata = {
   title: "Stocker",
@@ -14,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
