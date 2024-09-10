b14688b5a10b73251deb366a182f38717bc0afda
JohnPetros <joaopedro.nc@outlook.com>
2024-09-10 19:12:57 -0300

    ⚙️ config: set fastify error handler

--- apps/server/src/app/fastify/fastify-app.ts
+++ apps/server/src/app/fastify/fastify-app.ts
@@ -3,6 +3,14 @@ import Fastify, { type FastifyInstance } from 'fastify'
 import type { IServerApp } from '@stocker/core/interfaces'
 import { ENV } from '../../constants'
 import { ProductsRoutes } from './routes/products-routes'
+import {
+  AlreadyExistsError,
+  AppError,
+  AuthError,
+  NotFoundError,
+  ValidationError,
+} from '@stocker/core/errors'
+import { HTTP_STATUS_CODE } from '@stocker/core/constants'
 
 export class FastifyApp implements IServerApp {
   private readonly app: FastifyInstance
@@ -11,6 +19,7 @@ export class FastifyApp implements IServerApp {
     this.app = Fastify()
 
     this.registerRoutes()
+    this.setErrorHandler()
   }
 
   startServer() {
@@ -25,7 +34,44 @@ export class FastifyApp implements IServerApp {
       })
   }
 
-  stopServer() { }
+  stopServer() {}
+
+  private setErrorHandler() {
+    this.app.setErrorHandler((error, _, reply) => {
+      if (error instanceof AppError) {
+        console.error('Error title:', error.title)
+        console.error('Error message:', error.message)
+
+        const response = {
+          title: error.title,
+          message: error.message,
+        }
+
+        error.statusCode
+
+        if (error instanceof AuthError)
+          return reply.status(HTTP_STATUS_CODE.unauthorized).send(response)
+
+        if (error instanceof NotFoundError)
+          return reply.status(HTTP_STATUS_CODE.notFound).send(response)
+
+        if (error instanceof AlreadyExistsError)
+          return reply.status(HTTP_STATUS_CODE.conflict).send(response)
+
+        if (error instanceof ValidationError)
+          return reply.status(HTTP_STATUS_CODE.badRequest).send(response)
+
+        return reply.status(HTTP_STATUS_CODE.serverError).send(response)
+      }
+
+      console.error(error)
+
+      return reply.status(HTTP_STATUS_CODE.serverError).send({
+        title: 'Server Error',
+        message: error.message,
+      })
+    })
+  }
 
   private registerRoutes() {
     this.app.register(ProductsRoutes, { prefix: '/products' })
