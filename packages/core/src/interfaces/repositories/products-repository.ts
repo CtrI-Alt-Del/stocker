f6b80542296fd4de8e3c0102250b6c1285661fd3
0thigs <machinimavlogs@gmail.com>
2024-09-10 08:57:48 -0300

    📑interfaces: products

--- packages/core/src/interfaces/repositories/products-repository.ts
+++ packages/core/src/interfaces/repositories/products-repository.ts
@@ -1,5 +1,6 @@
 import type { Product } from '#domain/entities'
 
 export interface IProductsRepository {
-  add(product: Product): Promise<Product>
+  add(product: Product): Promise<Product>;
+  delete(idProduct: string): Promise<void>; 
 }
