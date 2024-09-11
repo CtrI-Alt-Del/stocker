8564985d6f5b9bcd9e6120574f0a1413ab52354e
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-09-11 18:00:42 -0300

    ✨feat: add get function

---
+++ packages/core/src/use-cases/products/get-product-use-case.ts
@@ -0,0 +1,22 @@
+import { IProductsRepository } from '../../interfaces'
+import { NotFoundError } from '../../errors'
+
+type Request = {
+  productId: string
+}
+
+export class GetProductUseCase {
+  private readonly productsRepository: IProductsRepository
+  constructor(productsRepository: IProductsRepository) {
+    this.productsRepository = productsRepository
+  }
+  
+  async execute({ productId }: Request) {
+    const product = await this.productsRepository.findById(productId)
+    if (!product) {
+      throw new NotFoundError('Produto não encontrado')
+    }
+
+    return product.dto
+  }
+}
