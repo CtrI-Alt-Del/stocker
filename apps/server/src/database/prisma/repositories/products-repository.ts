2e74682124e0eb28b655a289a35b78e9dc1ca23d
0thigs <machinimavlogs@gmail.com>
2024-09-13 17:59:02 -0300

    🐛fix: fix promise type

--- apps/server/src/database/prisma/repositories/products-repository.ts
+++ apps/server/src/database/prisma/repositories/products-repository.ts
@@ -2,7 +2,7 @@ import type { Product } from '@stocker/core/entities'
 import type { IProductsRepository } from '@stocker/core/interfaces'
 
 export class PrismaProductsRepository implements IProductsRepository {
-  async add(product: Product): Promise<Product> {
+  async add(product: Product): Promise<void> {
     throw new Error('Method not implemented')
   }
   async findById(productId: string): Promise<Product> {
