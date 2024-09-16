a117af4a4396153a462b7ecc3246d4a51b5b2071
0thigs <machinimavlogs@gmail.com>
2024-09-16 10:58:34 -0300

    🔧 refactor: update get product and list product controllers

---
+++ apps/server/src/api/controllers/products/get-products-controller.ts
@@ -0,0 +1,18 @@
+import type { IHttp } from '@stocker/core/interfaces'
+import { GetProductUseCase } from '@stocker/core/use-cases'
+
+import { productsRepository } from '@/database'
+
+type RouteParams = {
+  productId: string
+}
+
+export class GetProductsController {
+  async handle(http: IHttp) {
+    const { productId } = http.getRouteParams<RouteParams>()
+    const useCase = new GetProductUseCase(productsRepository)
+    const product = await useCase.execute({ productId })
+
+    return http.send(product)
+  }
+}
