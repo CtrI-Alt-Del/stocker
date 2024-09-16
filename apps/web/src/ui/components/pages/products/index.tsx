2287dec29c24e4ffbed160cfa01caafd1bffc149
kaufon <kauanfvale@gmail.com>
2024-09-16 18:25:53 -0300

    🚧in progress: add initial select filter

--- apps/web/src/ui/components/pages/products/index.tsx
+++ apps/web/src/ui/components/pages/products/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Input } from '@nextui-org/react'
+import { Button, Input, SelectItem, Select as SelectRoot } from '@nextui-org/react'
 import { Search } from 'lucide-react'
 
 export const ProductsPage = () => {
@@ -8,21 +8,33 @@ export const ProductsPage = () => {
       <div className='  flex-1 flex justify-between '>
         <div className='flex-1 max-w-96 space-y-2'>
           <h1 className='text-4xl font-black'>Produtos</h1>
-          <Input
-            placeholder='Pesquise por nome'
-            size='sm'
-            color='default'
-            radius='sm'
-            classNames={{
-              inputWrapper: ['bg-zinc-300', 'h-8'],
-              placeholder: 'text-[#8E8E8E]',
-            }}
-            endContent={<Search className='text-[#8E8E8E]' />}
-          />
+          <div className='flex flex-row justify-center items-center'>
+            <Input
+              placeholder='Pesquise por nome'
+              size='sm'
+              color='default'
+              radius='sm'
+              classNames={{
+                inputWrapper: ['bg-zinc-300', 'h-8'],
+                placeholder: 'text-[#8E8E8E]',
+              }}
+              endContent={<Search className='text-[#8E8E8E]' />}
+            />
+            <SelectRoot
+              className=''
+              selectionMode='multiple'
+              onSelectionChange={() => {
+                console.log('banana')
+              }}
+            >
+              <SelectItem>"Bananana"</SelectItem>
+              <SelectItem>"joao doria"</SelectItem>
+            </SelectRoot>
+          </div>
         </div>
         <Button
           variant='solid'
-          color = 'success'
+          color='success'
           size='lg'
           radius='sm'
           className='bg-[#EF5C31] hover:bg-orange-500 text-white font-black text-xl h-14 mr-9'
