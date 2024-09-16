b422e25674c0f2d3bff66821c6112265dd678637
kaufon <kauanfvale@gmail.com>
2024-09-16 18:04:40 -0300

    🚧in progress: initial products page

--- apps/web/src/ui/components/pages/products/index.tsx
+++ apps/web/src/ui/components/pages/products/index.tsx
@@ -1,4 +1,35 @@
+'use client'
+import { Button, Input } from '@nextui-org/react'
+import { Search } from 'lucide-react'
 
 export const ProductsPage = () => {
-  return <div>Products Page</div>
+  return (
+    <>
+      <div className='  flex-1 flex justify-between '>
+        <div className='flex-1 max-w-96 space-y-2'>
+          <h1 className='text-4xl font-black'>Produtos</h1>
+          <Input
+            placeholder='Pesquise por nome'
+            size='sm'
+            color='default'
+            radius='sm'
+            classNames={{
+              inputWrapper: ['bg-zinc-300', 'h-8'],
+              placeholder: 'text-[#8E8E8E]',
+            }}
+            endContent={<Search className='text-[#8E8E8E]' />}
+          />
+        </div>
+        <Button
+          variant='solid'
+          color = 'success'
+          size='lg'
+          radius='sm'
+          className='bg-[#EF5C31] hover:bg-orange-500 text-white font-black text-xl h-14 mr-9'
+        >
+          Adicionar
+        </Button>
+      </div>
+    </>
+  )
 }
