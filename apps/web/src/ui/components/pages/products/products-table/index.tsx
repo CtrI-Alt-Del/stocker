7763237a2c79725dbe144879b0d746cb58e2c7c2
kaufon <kauanfvale@gmail.com>
2024-09-18 14:18:23 -0300

    ♻️refactor: refactor table

--- apps/web/src/ui/components/pages/products/products-table/index.tsx
+++ apps/web/src/ui/components/pages/products/products-table/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { useProductsTableHook } from './use-products-table-hook'
 import {
   Table,
@@ -9,45 +9,7 @@ import {
   TableRow,
 } from '@nextui-org/react'
 
-  const COLUMNS = [
-    { name: 'NOME', uid: 'name' },
-    { name: 'DESCRIÇÃO', uid: 'description' },
-    { name: 'CODIGO', uid: 'code' },
-    { name: 'PREÇO', uid: 'price' },
-    { name: 'ESTOQUE MINIMO', uid: 'minimumStock' },
-    { name: 'ATIVO', uid: 'status' },
-  ]
-
 export const ProductsTable = () => {
-  const renderCell = useCallback((product, columnKey) => {
-    const cellValue = product[columnKey]
-
-    switch (columnKey) {
-      case 'name':
-        return (
-          <div className='flex items-center gap-2'>
-            <img
-              src={product.image}
-              alt={product.name}
-              className='w-8 h-8 rounded-full'
-            />
-            <p className='font-bold'>{cellValue}</p>
-          </div>
-        )
-      case 'description':
-        return <p className='text-sm text-gray-500'>{cellValue}</p>
-      case 'code':
-        return <p>{cellValue}</p>
-      case 'price':
-        return <p>${cellValue}</p>
-      case 'availableStock':
-        return <p>{cellValue}</p>
-      case 'minimumStock':
-        return <p>{cellValue}</p>
-      case 'status':
-        return <p>{cellValue}</p>
-    }
-  }, [])
   const [page, setPage] = useState<number>(1)
   const { products, loading } = useProductsTableHook(page)
   if (loading) {
@@ -56,22 +18,36 @@ export const ProductsTable = () => {
   return (
     <>
       <Table>
-        <TableHeader columns={COLUMNS}>
-          {(column) => (
-            <TableColumn
-              key={column.uid}
-              align={column.uid === 'actions' ? 'center' : 'start'}
-            >
-              {column.name}
-            </TableColumn>
-          )}
+        <TableHeader>
+          <TableColumn key={'name'}>NOME</TableColumn>
+          <TableColumn key={'description'}>DESCRIÇÃO</TableColumn>
+          <TableColumn key={'code'}>CODIGO</TableColumn>
+          <TableColumn key={'price'}>PREÇO</TableColumn>
+
+          <TableColumn key={'minimumStock'}>ESTOQUE MINIMO</TableColumn>
+
+          <TableColumn key={'status'}> ATIVO</TableColumn>
         </TableHeader>
         <TableBody items={products}>
-        {(item) => (
-        <TableRow>
-              <TableCell key={'NAME'}>{item.name}</TableCell>
-              </TableRow>
-        )}
+          {(item) => (
+            <TableRow>
+              <TableCell key={'NAME'}>
+                <div className='flex items-center gap-2'>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className='w-8 h-8 rounded-full'
+                  />
+                  <p className='font-bold'>{item.name}</p>
+                </div>
+              </TableCell>
+              <TableCell key={'description'}>{item.description}</TableCell>
+              <TableCell key={'code'}>{item.code}</TableCell>
+              <TableCell key={'price'}>{item.costPrice}</TableCell>
+              <TableCell key={'minimumStock'}>{item.minimumStock}</TableCell>
+              <TableCell key={'status'}>status</TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
     </>
