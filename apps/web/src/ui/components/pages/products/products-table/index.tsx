37c481690bbdc1816c0cfbf8d9e6dc926f927ec3
kaufon <kauanfvale@gmail.com>
2024-09-18 16:34:36 -0300

    🚧in progress: table using nuqs

--- apps/web/src/ui/components/pages/products/products-table/index.tsx
+++ apps/web/src/ui/components/pages/products/products-table/index.tsx
@@ -13,6 +13,7 @@ import {
   Tooltip,
 } from '@nextui-org/react'
 import { Edit, Search } from 'lucide-react'
+import { TableSearchComponent } from '@/ui/components/commons/search-component'
 
 export const ProductsTable = () => {
   const [page, setPage] = useState<number>(1)
@@ -43,24 +44,6 @@ export const ProductsTable = () => {
       setFilterByNameValue('')
     }
   }, [])
-  const tableTopComponent = useMemo(() => {
-    return (
-      <Input
-        placeholder='Pesquise por nome'
-        size='md'
-        color='default'
-        radius='sm'
-        classNames={{
-          inputWrapper: ['bg-zinc-100', 'h-12'],
-          placeholder: 'text-zinc-300',
-        }}
-        className='w-[40rem] max-w-96'
-        endContent={<Search className='text-zinc-300' />}
-        value={filterByNameValue}
-        onValueChange={onSearchChange}
-      />
-    )
-  }, [filterByNameValue, onSearchChange])
   if (loading) {
     return (
       <Spinner
@@ -74,8 +57,10 @@ export const ProductsTable = () => {
   return (
     <>
       <Table
+        arial-label="Products table"
+        
         shadow='none'
-        topContent={tableTopComponent}
+        topContent={<TableSearchComponent onSearchChange={onSearchChange} filterByNameValue={filterByNameValue}/>}
         topContentPlacement='outside'
         selectionMode='multiple'
         bottomContentPlacement='outside'
