d05c9701638091986d102a2830aee2eec877a8dd
kaufon <kauanfvale@gmail.com>
2024-09-18 13:17:38 -0300

    🚧in progress table component products

--- apps/web/src/ui/components/pages/products/products-table/index.tsx
+++ apps/web/src/ui/components/pages/products/products-table/index.tsx
@@ -1,34 +1,80 @@
-import { NextApiClient } from '@/api/next/next-api-client'
-import { ProductsService } from '@/api/services'
-import { ProductDto } from '@stocker/core/dtos'
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
+import { useProductsTableHook } from './use-products-table-hook'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/react'
+import { pages } from 'next/dist/build/templates/app-page'
+
+  const COLUMNS = [
+    { name: 'NOME', uid: 'name' },
+    { name: 'DESCRIÇÃO', uid: 'description' },
+    { name: 'CODIGO', uid: 'code' },
+    { name: 'PREÇO', uid: 'price' },
+    { name: 'ESTOQUE MINIMO', uid: 'minimumStock' },
+    { name: 'ATIVO', uid: 'status' },
+  ]
 
 export const ProductsTable = () => {
-  const apiClient = NextApiClient()
-  const [products, setProducts] = useState<ProductDto[]>([])
-  const [page, setPage] = useState<number>(1)
-  const [loading, setLoading] = useState<boolean>(true)
+  const renderCell = useCallback((product, columnKey) => {
+    const cellValue = product[columnKey]
 
-  const productsService = ProductsService(apiClient)
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setLoading(true)
-      try {
-        const response = await productsService.listProducts({ page })
-        setProducts(response.body.items)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setLoading(false)
-      }
+    switch (columnKey) {
+      case 'name':
+        return (
+          <div className='flex items-center gap-2'>
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-8 h-8 rounded-full'
+            />
+            <p className='font-bold'>{cellValue}</p>
+          </div>
+        )
+      case 'description':
+        return <p className='text-sm text-gray-500'>{cellValue}</p>
+      case 'code':
+        return <p>{cellValue}</p>
+      case 'price':
+        return <p>${cellValue}</p>
+      case 'availableStock':
+        return <p>{cellValue}</p>
+      case 'minimumStock':
+        return <p>{cellValue}</p>
+      case 'status':
+        return <p>{cellValue}</p>
     }
-
-    fetchProducts()
-  },[])
+  }, [])
+  const [page, setPage] = useState<number>(1)
+  const { products, loading } = useProductsTableHook(page)
+  if (loading) {
+    return <h1>Carregando tabela....</h1>
+  }
   return (
     <>
-      {console.log(products)}
+      <Table>
+        <TableHeader columns={COLUMNS}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === 'actions' ? 'center' : 'start'}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={products}>
+        {(item) => (
+        <TableRow>
+              <TableCell key={'NAME'}>{item.name}</TableCell>
+              </TableRow>
+        )}
+        </TableBody>
+      </Table>
     </>
   )
 }
