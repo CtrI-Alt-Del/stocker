b99cd47c289980a7bbc4e75bb53c5da376047d08
kaufon <kauanfvale@gmail.com>
2024-09-20 18:10:07 -0300

    ♻️refactor: add missing properties to movement dto

--- packages/core/src/dtos/inventory-movement-dto.ts
+++ packages/core/src/dtos/inventory-movement-dto.ts
@@ -1,8 +1,11 @@
 export type InventoryMovementDto = {
+  description?: string
   id?: string
   movementType: string
   itemsQuantity: number
   responsibleId: string
   productId: string
   registeredAt: Date
+  expireDate?: Date
+  batchCode?: string
 }
