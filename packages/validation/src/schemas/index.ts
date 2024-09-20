532f9715b0cebd0ee1f40e6d2d3711c38a3e4e74
kaufon <kauanfvale@gmail.com>
2024-09-20 18:10:58 -0300

    🖥hook: use inventory movement page

--- packages/validation/src/schemas/index.ts
+++ packages/validation/src/schemas/index.ts
@@ -1,10 +1,13 @@
 export { stringSchema } from './string-schema'
+export { dateTimeSchema } from "./date-time-schema"
+export { dateSchema } from "./date-schema"
+
 export { nameSchema } from './name-schema'
 export { integerSchema } from './integer-schema'
 export { fileSchema } from './file-schema'
 export { nonZeroIntegerSchema } from './non-zero-integer-schema'
 export { descriptionSchema } from './description-schema'
-export { booleanSchema } from './boolean.schema'
+export { booleanSchema } from './boolean-schema'
 export { idSchema } from './id-schema'
 export { appModeSchema } from './app-mode'
 export { urlSchema } from './urlSchema'
