d6784421ea265bda064636b3958a9820eb0b598f
kaufon <kauanfvale@gmail.com>
2024-09-20 18:08:28 -0300

    🖥hooks: inbound movement hook

--- apps/web/src/ui/components/pages/product-stock/inbound-movement/use-inbound-movement.ts
+++ apps/web/src/ui/components/pages/product-stock/inbound-movement/use-inbound-movement.ts
@@ -1,30 +1,62 @@
 
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { dateSchema, dateTimeSchema, descriptionSchema, integerSchema, stringSchema } from '@stocker/validation/schemas'
 import { z } from 'zod'
+import { useState } from 'react'
+import { useApi } from '@/ui/hooks'
+import { InventoryMovement } from '@stocker/core/entities'
 
 const registerInboundMovementFormSchema = z.object({
-  expireDate: z.string().datetime(),
-  creationDate: z.string().datetime(),
-  batchCode: z.string().min(1).max(50),
-  quantity: z.number().min(1),
-  description: z.string().max(500).optional()
+  expireDate: dateSchema,
+  registeredAt: dateSchema,
+  batchCode: stringSchema,
+  itemsQuantity: integerSchema,
+  description: descriptionSchema
 })
 
 type RegisterInboundMovementFormData = z.infer<typeof registerInboundMovementFormSchema>
 
-export function useRegisterInboundMovement() {
-  const { register, handleSubmit, formState: { errors } } = useForm<RegisterInboundMovementFormData>({
-    resolver: zodResolver(registerInboundMovementFormSchema),
-  })
+export function useRegisterInboundMovement(onSubmit: VoidFunction) {
+  const { control, formState, reset, register, handleSubmit } = useForm<RegisterInboundMovementFormData>({ resolver: zodResolver(registerInboundMovementFormSchema) })
 
-  function handleFormSubmit(data: RegisterInboundMovementFormData) {
-    console.log(data)
+  const [isSubmiting, setIsSubmitting] = useState(false)
+  const {inventoryMovementService: movementService} = useApi()
+
+  async function handleFormSubmit(formData: RegisterInboundMovementFormData){
+    setIsSubmitting(true)
+
+    const inboundMovement = InventoryMovement.create({
+      batchCode: formData.batchCode,
+      registeredAt: formData.registeredAt,
+      itemsQuantity: formData.itemsQuantity,
+      expireDate: formData.expireDate,
+      responsibleId: '123',
+      productId: "idk",
+      movementType: 'inbound',
+      description: formData.description
+       
+    })
+    const response = await movementService.registerInboundMovement(inboundMovement)
+
+    if (response.isFailure) {
+      alert(response.errorMessage)
+      
+    }
+    if (response.isSuccess) {
+      reset()
+      onSubmit()
+      
+    }
+    setIsSubmitting(false)
   }
-
-  return {
+  return{
+    control,
+    errors: formState.errors,
+    isSubmiting,
     register,
-    handleSubmit: handleSubmit(handleFormSubmit),
-    errors,
+    handleSubmit: handleSubmit(handleFormSubmit)
   }
+
+
 }
