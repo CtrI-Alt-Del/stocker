494ee70be0d1f38ae5c391a1d02725ca7203138a
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-09-24 10:41:30 -0300

    🛣 routes: report stock level route

--- apps/server/src/app/fastify/routes/index.ts
+++ apps/server/src/app/fastify/routes/index.ts
@@ -1,3 +1,4 @@
 export { ProductsRoutes } from './products-routes'
 export { InventoryMovementRoutes } from './inventory-movement-routes'
 export { FileStorageRoutes } from './file-storage-routes'
+export { StockLevelRoutes } from './report-stock-level-routes'
