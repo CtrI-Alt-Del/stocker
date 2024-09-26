ffd9232408a724727c868ed4cfe3f21e6e29c054
FaelSantoss <silva.faelm@gmail.com>
2024-09-26 07:56:08 -0300

    👌review: review page not found

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,7 +1,4 @@
-"use client";
 import { NotFoundPage } from "@/ui/components/pages/notfound";
-
- // Adiciona esta diretiva no topo do arquivo
 
 export default function NotFound() {
   return <NotFoundPage />
