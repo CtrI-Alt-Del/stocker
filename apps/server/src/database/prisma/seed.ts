ed45bcbfd57fdfdfbc36227c5be173bb51970dce
kaufon <kauanfvale@gmail.com>
2024-10-01 15:23:04 -0300

    🤡mocks: mock for products and its batchs and movements

--- apps/server/src/database/prisma/seed.ts
+++ apps/server/src/database/prisma/seed.ts
@@ -12,6 +12,7 @@ const COMPANY_ID = 'eceda392-06df-4ed2-8c90-db6bf1e38830'
 const MANAGER_ID = '29fcf7a0-5ee3-4cb0-b36e-ecc825f1cdaa'
 const CATEGORY_ID = '602f6307-a60a-4825-b57d-873b97fe2bca'
 const PRODUCT_ID = 'ca479caf-31f7-48ed-bc0c-7e3115d04e32'
+const CONNECTION_POOL_SIZE = 5
 
 async function seed() {
   await prisma.$executeRawUnsafe(`
@@ -42,28 +43,48 @@ async function seed() {
       company_id: COMPANY_ID,
     },
   })
+
   const fakeProduct = ProductsFaker.fake({
     id: PRODUCT_ID,
     categoryId: CATEGORY_ID,
     companyId: COMPANY_ID,
   })
   await productsRepository.add(fakeProduct)
-  const fakeBatches = BatchesFaker.fakeMany({
-    productId: PRODUCT_ID,
-  })
-  for (const batch of fakeBatches) {
-    await batchesRepository.add(batch)
+
+  const productPromises = []
+  for (let index = 0; index < 50; index++) {
+    const product = ProductsFaker.fake({
+      id: faker.string.uuid(),
+      categoryId: CATEGORY_ID,
+      companyId: COMPANY_ID,
+    })
+    productPromises.push(
+      productsRepository.add(product).then(async () => {
+        const fakeBatches = BatchesFaker.fakeMany({
+          productId: product.id,
+        })
+        const batchPromises = fakeBatches.map((batch) => batchesRepository.add(batch))
+
+        const fakeInventoryMovements = InventoryMovementsFaker.fakeMany({
+          product: { id: product.id },
+          responsible: { id: MANAGER_ID },
+        })
+        const movementsPromise = fakeInventoryMovements.map((movement) =>
+          inventorymovementRepository.add(movement),
+        )
+
+        await Promise.all([...batchPromises, ...movementsPromise])
+      }),
+    )
+
+    if (productPromises.length >= CONNECTION_POOL_SIZE) {
+      await Promise.allSettled(productPromises)
+      productPromises.length = 0 
+    }
   }
 
-  const fakeInventoryMovements = InventoryMovementsFaker.fakeMany(
-    faker.number.int({ min: 0, max: 100 }),
-    {
-      product: { id: PRODUCT_ID },
-      responsible: {id: MANAGER_ID}
-    },
-  )
-  for (const movement of fakeInventoryMovements) {
-    await inventorymovementRepository.add((movement))
+  if (productPromises.length > 0) {
+    await Promise.allSettled(productPromises)
   }
 }
 
