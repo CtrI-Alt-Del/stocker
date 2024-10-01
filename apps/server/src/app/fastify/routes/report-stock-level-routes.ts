9e20329c22d9020a36e9c84184c41296b4be66b4
0thigs <machinimavlogs@gmail.com>
2024-10-01 20:57:34 -0300

    🛣️routes: create most trending products routes

--- apps/server/src/app/fastify/routes/report-stock-level-routes.ts
+++ apps/server/src/app/fastify/routes/report-stock-level-routes.ts
@@ -1,12 +1,19 @@
 import { FastifyInstance } from "fastify"
 import { FastifyHttp } from "../fastify-http"
 import { ReportStockLevelController } from "@/api/controllers/reports"
+import { MostTrendingProductsController } from "@/api/controllers/products"
 
 export const ReportsRoutes = async (app: FastifyInstance) => {
   const reportStockLevelController = new ReportStockLevelController()
+  const mostTrendingProductsController = new MostTrendingProductsController()
 
   app.get('/stock-level', async (request, response) => {
     const http = new FastifyHttp(request, response)
     return reportStockLevelController.handle(http)
+  })
+
+  app.get('/most-trending-products', async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return mostTrendingProductsController.handle(http)
   })
 }
