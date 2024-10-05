b06ecdfb8049fac072a596d373891dfe524f9416
0thigs <machinimavlogs@gmail.com>
2024-10-04 23:55:55 -0300

    ✨ use cases: index inventory to csv file use case

--- packages/core/src/use-cases/reports/index.ts
+++ packages/core/src/use-cases/reports/index.ts
@@ -1,3 +1,4 @@
 export { ReportStockLevelUseCase } from './report-stock-level-use-case'
 export { ReportMostTrendingProductsUseCase } from './report-most-trending-products-use-case'
 export { ExportMostTrendingProductsToCsvFileUseCase } from './export-most-trending-products-use-case'
+export { ExportInventoryToCsvFileUseCase } from './export-inventory-to-csv-file-use-case'
