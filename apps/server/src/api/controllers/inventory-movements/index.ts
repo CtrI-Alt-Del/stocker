591710d28854755b39c33f2f80ead71fc82f42dc
FaelSantoss <silva.faelm@gmail.com>
2024-10-06 10:52:25 -0300

    🖥️ controllers: create controller for report annul outbound inventory movements

--- apps/server/src/api/controllers/inventory-movements/index.ts
+++ apps/server/src/api/controllers/inventory-movements/index.ts
@@ -1,3 +1,4 @@
 export { ListInventoryMovementsController } from './list-inventory-movements-controller'
 export { RegisterInboundInventoryMovementController } from './register-inbound-inventory-movement-controller'
 export { RegisterOutboundInventoryMovementController } from './register-outbound-inventory-movement-controller'
+export { ReportAnnualOutboundInventorymovementsController } from './report-annual-outbound-inventorymovements-controller'
---
+++ apps/server/src/api/controllers/inventory-movements/report-annual-outbound-inventorymovements-controller.ts
@@ -0,0 +1,17 @@
+import { ReportAnnualOutboundInventorymovementsUseCase } from '@stocker/core/use-cases'
+import type { IHttp } from '@stocker/core/interfaces'
+import { HTTP_STATUS_CODE } from '@stocker/core/constants'
+import { inventorymovementRepository, productsRepository } from '@/database'
+import type { FindByDateRangeParams } from '@stocker/core/types'
+
+export class ReportAnnualOutboundInventorymovementsController {
+  async handle(http: IHttp) {
+    const queryParams = http.getQueryParams<FindByDateRangeParams>()
+    
+    const useCase = new ReportAnnualOutboundInventorymovementsUseCase(inventorymovementRepository, productsRepository)
+    const result = await useCase.execute({
+      productId: queryParams.productId,
+    })
+    return http.send(result, HTTP_STATUS_CODE.ok)
+  }
+}
