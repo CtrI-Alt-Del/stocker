fd559c79fe3c16a144546be2f8b4454984b562ff
kaufon <kauanfvale@gmail.com>
2024-10-10 20:07:22 -0300

    👥interface: report service

--- packages/core/src/interfaces/services/reports-services.ts
+++ packages/core/src/interfaces/services/reports-services.ts
@@ -1,15 +1,15 @@
 import type {
+    AnnualInventoryMovementsDto,
   ProductDto,
   StockLevelReportDto,
   WeeklyInventoryMovementsDto,
 } from '../../dtos'
-import { AnnualInventoryMovementsDto } from '../../dtos/annual-inventory-movements-dto'
 import type { ApiResponse, PaginationResponse } from '../../responses'
 import type { ProducsStocksListParams } from '../../types'
 import type { MostTrendingProductsListParams } from '../../types/most-trending-products-list-params'
 
 export interface IReportsService {
-  reportAnualInventoryMovements(productID?:string): Promise<ApiResponse<AnnualInventoryMovementsDto>>
+  reportAnualInventoryMovements(productID?:string): Promise<ApiResponse<AnnualInventoryMovementsDto[]>>
   reportSummary(): Promise<ApiResponse<any>>
   reportStockLevel(): Promise<ApiResponse<StockLevelReportDto>>
   reportMostTrendingProducts(
