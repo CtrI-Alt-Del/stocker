84724eed36f922e90706c55def2243b98ae4d824
kaufon <kauanfvale@gmail.com>
2024-10-10 20:07:32 -0300

    🐛fix: import  type

--- packages/core/src/dtos/index.ts
+++ packages/core/src/dtos/index.ts
@@ -1,4 +1,5 @@
 export type { ProductDto } from './product-dto'
+export type {AnnualInventoryMovementsDto} from './annual-inventory-movements-dto'
 export type { BatchDto } from './batch-dto'
 export type { InventoryMovementDto } from './inventory-movement-dto'
 export type { UserDto } from './user-dto'
