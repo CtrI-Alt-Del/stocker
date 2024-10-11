26d7513c1bba6b4d3b2ac01fd84c85584d847dec
0thigs <machinimavlogs@gmail.com>
2024-10-11 15:31:58 -0300

    ♲refactor: export prisma category types

--- apps/server/src/database/prisma/types/index.ts
+++ apps/server/src/database/prisma/types/index.ts
@@ -1,3 +1,4 @@
 export type { PrismaBatch } from './prisma-batch'
 export type { PrismaProduct } from './prisma-product'
 export type { PrismaInventoryMovement } from './prisma-inventory-movement'
+export type { PrismaCategory } from './prisma-categories'
