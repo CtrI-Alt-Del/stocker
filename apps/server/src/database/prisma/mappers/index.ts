ae6eda674121907c2c11ef2b5d357139fe4924ed
0thigs <machinimavlogs@gmail.com>
2024-10-11 15:37:47 -0300

    ♲refactor: export categories mapper

--- apps/server/src/database/prisma/mappers/index.ts
+++ apps/server/src/database/prisma/mappers/index.ts
@@ -1,3 +1,4 @@
 export { PrismaProductMapper } from './prisma-product-mapper'
 export { PrismaInventoryMovementsMapper } from './prisma-inventory-movements-mapper'
 export { PrismaBatchesMapper } from './prisma-batches-mapper'
+export { PrismaCategoriesMapper } from './prisma-categories-mapper'
