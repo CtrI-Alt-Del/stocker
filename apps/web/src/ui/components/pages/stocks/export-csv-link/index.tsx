0354c61e9e43f86d05c8068c17807774bb63ae92
JoaoGabrielGarcia <joaog.oliveiragarcia@gmail.com>
2024-10-11 11:29:35 -0300

    ✨ Feat: Inventory CSV Button

---
+++ apps/web/src/ui/components/pages/stocks/export-csv-link/index.tsx
@@ -0,0 +1,17 @@
+import { Icon } from '@/ui/components/commons/icon'
+import { Button, Link } from '@nextui-org/react'
+
+export const ExportCsvLink = () => {
+    
+  return (
+    <Button
+      as={Link}
+      color='primary'
+      radius='md'
+      href='reports/inventory/csv'
+      endContent={<Icon name='download' />}
+    >
+      Exportar CSV
+    </Button>
+  )
+}
