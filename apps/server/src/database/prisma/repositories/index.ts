10d6bdfd33298a63787f8020b83c5cf989c15178
0thigs <machinimavlogs@gmail.com>
2024-10-11 15:33:11 -0300

    ♲refactor: add export respository

--- apps/server/src/database/prisma/repositories/index.ts
+++ apps/server/src/database/prisma/repositories/index.ts
@@ -1,3 +1,4 @@
 export { PrismaProductsRepository } from './prisma-products-repository'
 export { PrismaBatchesRepository } from './prisma-batches-repository'
 export { PrismaInventoryMovementsRepository } from './prisma-inventory-movements-repository'
+export { PrismaCategoriesRepository } from './prisma-categories-repository'
