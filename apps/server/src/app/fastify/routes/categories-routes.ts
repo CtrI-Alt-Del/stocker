ac4fac484f52e7b96a45451ddf08866761fbe668
FaelSantoss <silva.faelm@gmail.com>
2024-10-14 12:14:17 -0300

    ✨ feat: create route delete category

--- apps/server/src/app/fastify/routes/categories-routes.ts
+++ apps/server/src/app/fastify/routes/categories-routes.ts
@@ -1,11 +1,12 @@
-import { ListCategoryController, RegisterCategoryController } from '@/api/controllers/categories'
+import { DeleteCategoyController, ListCategoryController, RegisterCategoryController } from '@/api/controllers/categories'
 import type { FastifyInstance } from 'fastify'
 import { FastifyHttp } from '../fastify-http'
 
 export const CategoriesRoutes = async (app: FastifyInstance) => {
     const registerCategoryController = new RegisterCategoryController()
     const listCategoryController = new ListCategoryController()
-    
+    const deleteCategoyController = new DeleteCategoyController()
+
     app.post('/', async (request, response) => {
         const http = new FastifyHttp(request, response)
         return registerCategoryController.handle(http)
@@ -14,5 +15,10 @@ export const CategoriesRoutes = async (app: FastifyInstance) => {
     app.get('/', async (request, response) => {
         const http = new FastifyHttp(request, response)
         return listCategoryController.handle(http)
+    })
+
+    app.delete('/:categoryId', async (request, response) => {
+        const http = new FastifyHttp(request, response)
+        return deleteCategoyController.handle(http)
     })
 }
