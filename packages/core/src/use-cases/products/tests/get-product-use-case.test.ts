534723f5d38a32c8c4ed92b812329fd507242a0a
JohnPetros <joaopedro.nc@outlook.com>
2024-10-15 09:18:38 -0300

    🧪 tests: should not get inactive product

--- packages/core/src/use-cases/products/tests/get-product-use-case.test.ts
+++ packages/core/src/use-cases/products/tests/get-product-use-case.test.ts
@@ -1,7 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { GetProductUseCase } from '../get-product-use-case'
 import { ProductsRepositoryMock } from '../../../../__tests__/mocks/repositories'
-import { NotFoundError } from '../../../errors'
+import { NotAllowedError, NotFoundError } from '../../../errors'
 import { ProductsFaker } from '../../../../__tests__/fakers'
 
 let useCase: GetProductUseCase
@@ -13,7 +13,7 @@ describe('Get product use case', () => {
     useCase = new GetProductUseCase(productsRepository)
   })
 
-  it('should not get product if the product does not exist', async () => {
+  it('should not get a product if the product does not exist', async () => {
     const fakeProduct = ProductsFaker.fake()
 
     expect(async () => {
@@ -21,7 +21,16 @@ describe('Get product use case', () => {
     }).rejects.toThrowError(NotFoundError)
   })
 
-  it('should not get product', async () => {
+  it('should not get a product that is not active', async () => {
+    const fakeProduct = ProductsFaker.fake({ isActive: false })
+    await productsRepository.add(fakeProduct)
+
+    expect(async () => {
+      await useCase.execute({ productId: fakeProduct.id })
+    }).rejects.toThrowError(NotAllowedError)
+  })
+
+  it('should get a product', async () => {
     const fakeProduct = ProductsFaker.fake()
     await productsRepository.add(fakeProduct)
 
