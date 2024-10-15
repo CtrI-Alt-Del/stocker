8ae97de3f5a95bad59996ce318c60a5821b549e0
JohnPetros <joaopedro.nc@outlook.com>
2024-10-15 15:19:06 -0300

    🧪 tests: report inventory use case

--- packages/core/src/use-cases/reports/report-inventory-summary-use-case.ts
+++ packages/core/src/use-cases/reports/report-inventory-summary-use-case.ts
@@ -1,20 +1,29 @@
-import { IBatchesRepository, IInventoryMovementsRepository } from "../../interfaces"
+import type { IBatchesRepository, IInventoryMovementsRepository } from '../../interfaces'
 
 export class ReportInventorySummaryUseCase {
   private readonly batchesRepository: IBatchesRepository
   private readonly inventoryMovementsRepository: IInventoryMovementsRepository
-  constructor(batchesRepository: IBatchesRepository, inventoryMovementsRepository: IInventoryMovementsRepository) {
-    this.batchesRepository = batchesRepository, this.inventoryMovementsRepository = inventoryMovementsRepository
+  constructor(
+    batchesRepository: IBatchesRepository,
+    inventoryMovementsRepository: IInventoryMovementsRepository,
+  ) {
+    this.batchesRepository = batchesRepository
+    this.inventoryMovementsRepository = inventoryMovementsRepository
   }
 
   async execute() {
-    const [batchescount, itemscount, inboundinventorymovementscount, outboundinventorymovementscount] = await Promise.all([
+    const [
+      batchescount,
+      itemscount,
+      inboundinventorymovementscount,
+      outboundinventorymovementscount,
+    ] = await Promise.all([
       this.batchesRepository.count(),
       this.batchesRepository.countItems(),
       this.inventoryMovementsRepository.countInbound(),
       this.inventoryMovementsRepository.countOutbound(),
     ])
-    
+
     return {
       batchescount: batchescount,
       itemscount: itemscount,
---
+++ packages/core/src/use-cases/reports/tests/report-inventory-use-case.test.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { ProductsRepositoryMock } from '../../../../__tests__/mocks/repositories'
+import { ReportInventorysUseCase } from '../report-inventory-use-case'
+import { PAGINATION } from '../../../constants'
+import { ProductsFaker } from '../../../../__tests__/fakers'
+import type { Product } from '../../../domain/entities'
+
+let useCase: ReportInventorysUseCase
+let productsRepository: ProductsRepositoryMock
+let fakeProducts: Product[]
+
+describe('Report weekly inventory movements use case', () => {
+  beforeEach(async () => {
+    productsRepository = new ProductsRepositoryMock()
+
+    fakeProducts = ProductsFaker.fakeMany(20)
+    for (const fakeProduct of fakeProducts) {
+      await productsRepository.add(fakeProduct)
+    }
+
+    useCase = new ReportInventorysUseCase(productsRepository)
+  })
+
+  it(`should list ${PAGINATION.itemsPerPage} products with inventory movements per page`, async () => {
+    const pagination = await useCase.execute({ page: 1 })
+
+    expect(pagination.items).toEqual(
+      fakeProducts.slice(0, 10).map((fakeProduct) => fakeProduct.dto),
+    )
+  })
+
+  it(`should list ${PAGINATION.itemsPerPage} products with inventory movements per page`, async () => {
+    const pagination = await useCase.execute({ page: 2 })
+
+    expect(pagination.items).toEqual(
+      fakeProducts.slice(10, 20).map((fakeProduct) => fakeProduct.dto),
+    )
+  })
+})
