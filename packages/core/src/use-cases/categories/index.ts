23d22647496b7a3901d9222877ab07d4a99d67e9
FaelSantoss <silva.faelm@gmail.com>
2024-10-16 10:35:42 -0300

    ✨ feat: create use case for category update

--- packages/core/src/use-cases/categories/index.ts
+++ packages/core/src/use-cases/categories/index.ts
@@ -1,3 +1,4 @@
 export { RegisterCategoryUseCase } from './register-category-use-case'
 export { ListCategoryUseCase } from './list-category-use-case'
 export { DeleteCategoryUseCase } from './delete-category-use-case'
+export { UpdateCategoryUseCase } from './update-category-use-case'
