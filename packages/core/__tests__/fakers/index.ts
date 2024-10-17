21d77c185e101ce33e94f670ddcaefe735d937fc
JoaoGabrielGarcia <joaog.oliveiragarcia@gmail.com>
2024-10-17 11:31:35 -0300

    🥸 Faker: Add category-faker and CategoriesFaker into the seed.ts

--- packages/core/__tests__/fakers/index.ts
+++ packages/core/__tests__/fakers/index.ts
@@ -1,3 +1,4 @@
 export { ProductsFaker } from './products-faker'
 export { BatchesFaker } from './batch-faker'
 export { InventoryMovementsFaker } from './movements-faker'
+export { CategoriesFaker } from './category-faker'
