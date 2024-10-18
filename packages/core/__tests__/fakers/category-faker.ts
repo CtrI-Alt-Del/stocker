ce96f85457edf247361c3fbc322eec783db2c78b
kaufon <kauanfvale@gmail.com>
2024-10-18 16:34:44 -0300

    🐛fix:fix categories faker

--- packages/core/__tests__/fakers/category-faker.ts
+++ packages/core/__tests__/fakers/category-faker.ts
@@ -1,28 +1,45 @@
-import { fakerPT_BR as faker } from '@faker-js/faker'
+import { base, fakerPT_BR as faker } from '@faker-js/faker'
 import { Category } from '../../src/domain/entities'
 import type { CategoryDto } from '../../src/dtos'
 
 export class CategoriesFaker {
-    static fake(categoryDto?: Partial<CategoryDto>) {
-        return Category.create(CategoriesFaker.fakeDto(categoryDto))
-    }
-
-    static fakeDto(baseDto?: Partial<CategoryDto>): CategoryDto {
-        return {
-            id: faker.string.uuid(),
-            name: faker.commerce.productName(),
-            companyId: faker.string.uuid(),
-            // subcategories: [],
-            ...baseDto,     
-        }
-    }
-    
-  static fakeMany(count = 10, baseDto?: Partial<CategoryDto>) {
-    return Array.from({ length: count }).map(() => CategoriesFaker.fake(baseDto))
+  static fake(categoryDto?: Partial<CategoryDto>, depth = 0) {
+    return Category.create(CategoriesFaker.fakeDto(categoryDto, depth))
   }
 
-  static fakeManyDto(count = 10, baseDto?: Partial<CategoryDto>) {
-    return Array.from({ length: count }).map(() => CategoriesFaker.fakeDto(baseDto))
+  static fakeDto(
+    baseDto?: Partial<CategoryDto>,
+    depth = 0,
+    parentId?: string,
+  ): CategoryDto {
+    const id = faker.string.uuid()
+
+    const subCategories =
+      depth < 1 ? CategoriesFaker.fakeManyDto(2, {}, depth + 1, id) : []
+
+    return {
+      id: id,
+      name: faker.commerce.productName(),
+      companyId: faker.string.uuid(),
+      parentCategoryId: parentId || undefined,
+      subCategories: subCategories,
+      ...baseDto,
+    }
+  }
+
+  static fakeMany(count = 10, baseDto?: Partial<CategoryDto>, depth = 0) {
+    return Array.from({ length: count }).map(() => CategoriesFaker.fake(baseDto, depth))
+  }
+
+  static fakeManyDto(
+    count = 10,
+    baseDto?: Partial<CategoryDto>,
+    depth = 0,
+    parentId?: string,
+  ) {
+    return Array.from({ length: count }).map(() =>
+      CategoriesFaker.fakeDto(baseDto, depth, parentId),
+    )
   }
 }
 
