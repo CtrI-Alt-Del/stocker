ad5909305e0929dfb7699a3b1b8afc0f4398804d
FaelSantoss <silva.faelm@gmail.com>
2024-10-19 17:41:29 -0300

    🤡 mocks: add category repository

---
+++ packages/core/__tests__/mocks/repositories/category-repository-mock.ts
@@ -0,0 +1,49 @@
+import type { Category } from '../../../src/domain/entities'
+import { NotFoundError } from '../../../src/errors'
+import type { ICategoriesRepository } from '../../../src/interfaces'
+import type { CategoriesListParams } from '../../../src/types'
+
+export class CategoriesRepositoryMock implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async findById(categoryId: string): Promise<Category | null> {
+    const category = this.categories.find((cat) => cat.id === categoryId) ?? null
+    return category
+  }
+
+  async findMany({ page }: CategoriesListParams): Promise<Category[]> {
+    const startIndex = (page - 1) * 10 // 10 pode ser o valor do PAGINATION.itemsPerPage
+    return this.categories.slice(startIndex, startIndex + 10)
+  }
+
+  async add(category: Category): Promise<void> {
+    this.categories.push(category)
+  }
+
+  async count(): Promise<number> {
+    return this.categories.length
+  }
+
+  async update(category: Category): Promise<void> {
+    this.categories = this.categories.map((existingCategory) =>
+      existingCategory.id === category.id ? category : existingCategory
+    )
+  }
+
+  async deleteById(categoryId: string): Promise<void> {
+    const categoryToDelete = this.categories.find((cat) => cat.id === categoryId);
+
+    if (!categoryToDelete) {
+      throw new NotFoundError();
+    }
+    const subCategoryIds = categoryToDelete.dto.subCategories.map(subCat => subCat.id);
+    if (subCategoryIds) {
+      this.categories = this.categories.filter(cat => {
+        return !subCategoryIds.includes(cat.id);
+      });
+    }
+    
+    this.categories = this.categories.filter(cat => cat.id !== categoryId);
+    
+  }
+}
