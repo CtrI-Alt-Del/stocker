9fa322d1a34b92ddcb22ae5b8438895d0d42ddd9
kaufon <kauanfvale@gmail.com>
2024-10-23 12:08:21 -0300

    🐛fix: use-breakpoint

--- apps/web/src/ui/hooks/use-breakpoint.ts
+++ apps/web/src/ui/hooks/use-breakpoint.ts
@@ -1,17 +1,19 @@
-import { useMediaQuery } from 'usehooks-ts'
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 
 export function useBreakpoint() {
-  const xs = useMediaQuery('(min-width: 440px)')
-  const sm = useMediaQuery('(min-width: 640px)')
-  const md = useMediaQuery('(min-width: 768px)')
-  const lg = useMediaQuery('(min-width: 1024px)')
-  const xl = useMediaQuery('(min-width: 1280px)')
+  const [isClient, setIsClient] = useState(false);
 
-  return {
-    xs,
-    sm,
-    md,
-    lg,
-    xl,
-  }
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const xs = useMediaQuery('(min-width: 440px)');
+  const sm = useMediaQuery('(min-width: 640px)');
+  const md = useMediaQuery('(min-width: 768px)');
+  const lg = useMediaQuery('(min-width: 1024px)');
+  const xl = useMediaQuery('(min-width: 1280px)');
+
+  return isClient ? { xs, sm, md, lg, xl } : { xs: false, sm: false, md: false, lg: false, xl: false };
 }
+
