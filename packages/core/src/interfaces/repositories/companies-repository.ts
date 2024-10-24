6fe24da5cb69d933da1a1073e6977a4723ef6c55
FaelSantoss <silva.faelm@gmail.com>
2024-10-24 07:46:01 -0300

    🗄️ repositories: create Companies Repository

--- packages/core/src/interfaces/repositories/companies-repository.ts
+++ packages/core/src/interfaces/repositories/companies-repository.ts
@@ -2,7 +2,7 @@ import type { Company } from "../../domain/entities";
 
 export interface ICompaniesRepository {
 	add(company: Company): Promise<void>
-	findById(companyId: string): Promise<Company>
+	findById(companyId: string): Promise<Company | null>
 	update(company: Company, companyId: string): Promise<Company>
 	delete(companyId: string): Promise<void>
 }
