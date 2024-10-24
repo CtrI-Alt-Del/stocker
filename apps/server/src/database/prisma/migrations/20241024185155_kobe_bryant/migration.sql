8686dd71f1de7498e60946535a444b53d4adc7bb
kaufon <kauanfvale@gmail.com>
2024-10-24 17:23:24 -0300

    💾database: migrate db

---
+++ apps/server/src/database/prisma/migrations/20241024185155_kobe_bryant/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the `notifications` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "notifications" DROP CONSTRAINT "notifications_batch_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "notifications" DROP CONSTRAINT "notifications_product_id_fkey";
+
+-- DropTable
+DROP TABLE "notifications";
+
+-- CreateTable
+CREATE TABLE "stock-level-notifications" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "stock-level-notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "expiration-date-notification" (
+    "id" TEXT NOT NULL,
+    "batch_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "expiration-date-notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "stock-level-notifications" ADD CONSTRAINT "stock-level-notifications_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "expiration-date-notification" ADD CONSTRAINT "expiration-date-notification_batch_id_fkey" FOREIGN KEY ("batch_id") REFERENCES "batches"("id") ON DELETE CASCADE ON UPDATE CASCADE;
