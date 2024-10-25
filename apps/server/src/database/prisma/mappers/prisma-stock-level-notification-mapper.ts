d5a4139be5b83b0d072bf7b2cf805305b4451830
0thigs <machinimavlogs@gmail.com>
2024-10-25 17:40:38 -0300

    ✨feat: create stock level notification mapper

---
+++ apps/server/src/database/prisma/mappers/prisma-stock-level-notification-mapper.ts
@@ -0,0 +1,28 @@
+import { StockLevelNotification } from '@stocker/core/entities'
+import type { PrismaStockLevelNotification } from '../types'
+
+export class PrismaStockLevelNotificationMapper {
+  toDomain(stockLevelNotification: PrismaStockLevelNotification): StockLevelNotification {
+    return StockLevelNotification.create({
+      product: {
+        id: stockLevelNotification.product_id,
+        name: stockLevelNotification.Product.name,
+        code: stockLevelNotification.Product.code,
+      },
+      createdAt: stockLevelNotification.created_at,
+    })
+  }
+
+  toPrisma(stockLevelNotification: StockLevelNotification): PrismaStockLevelNotification {
+    return {
+      id: stockLevelNotification.id,
+      product_id: stockLevelNotification.product.id,
+      created_at: stockLevelNotification.createdAt,
+      Product: {
+        id: stockLevelNotification.product.id,
+        name: stockLevelNotification.product.name,
+        code: stockLevelNotification.product.code,
+      },
+    }
+  }
+}
