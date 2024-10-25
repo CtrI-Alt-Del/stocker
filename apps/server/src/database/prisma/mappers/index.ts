65155d29afaa098a2305ad70e5526445908fad21
FaelSantoss <silva.faelm@gmail.com>
2024-10-25 14:11:13 -0300

    ✨ feat: create users mapper

--- apps/server/src/database/prisma/mappers/index.ts
+++ apps/server/src/database/prisma/mappers/index.ts
@@ -3,3 +3,4 @@ export { PrismaInventoryMovementsMapper } from './prisma-inventory-movements-map
 export { PrismaBatchesMapper } from './prisma-batches-mapper'
 export { PrismaCategoriesMapper } from './prisma-categories-mapper'
 export { PrismaCompaniesMapper } from './prisma-companies-mapper'
+export { PrismaUsersMapper } from './prisma-users-mapper'
---
+++ apps/server/src/database/prisma/mappers/prisma-users-mapper.ts
@@ -0,0 +1,25 @@
+import { User } from '@stocker/core/entities'
+import type { PrismaUser } from '../types'
+
+export class PrismaUsersMapper {
+  toDomain(prismaUser: PrismaUser): User {
+    const user = User.create({
+      id: prismaUser.id,
+      name: prismaUser.name,
+      email: prismaUser.email,
+      role: prismaUser.role.toLowerCase() as 'admin' | 'manager' | 'employee',
+      companyId: prismaUser.company_id
+    })
+    return user
+  }
+
+  toPrisma(user: User): PrismaUser {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role.toUpperCase() as 'ADMIN' | 'MANAGER' | 'EMPLOYEE',
+      company_id: user.companyId
+    }
+  }
+}
