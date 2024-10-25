cf5f07ac0738cf4303c1e5c1508b1c3a9c5c3aaf
kaufon <kauanfvale@gmail.com>
2024-10-25 16:49:49 -0300

    🧱componenets: update employee form

---
+++ apps/web/src/ui/components/pages/employees/update-employee-form/index.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { useRef } from 'react'
+import { Controller } from 'react-hook-form'
+import {
+  Button,
+  Divider,
+  Input,
+  Select,
+  SelectItem,
+  Switch,
+  Textarea,
+} from '@nextui-org/react'
+import { useUpdateEmployeeForm } from './use-update-employee-form'
+import type { User } from '@stocker/core/entities'
+
+type UpdateEmployeeCategoryProps = {
+  employee: User
+
+  onCancel: VoidFunction
+  onSubmit: VoidFunction
+}
+
+export const UpdateEmployeeForm = ({
+  onSubmit,
+  onCancel,
+  employee,
+}: UpdateEmployeeCategoryProps) => {
+  const { register, errors, handleSubmit, isDirty, isSubmiting, control } =
+    useUpdateEmployeeForm({
+      onSubmit,
+      onCancel,
+      employee,
+    })
+
+  return (
+    <form onSubmit={handleSubmit} encType='multiform/form-data' className='space-y-6'>
+      <div className='w-full'>
+        <div className='grid grid-cols-1 w-full  grid-rows-3 gap-6'>
+          <Input
+            label='Nome'
+            isRequired
+            isInvalid={Boolean(errors.name)}
+            errorMessage={errors.name?.message}
+            {...register('name')}
+          />
+          <Input
+            label='E-mail'
+            isRequired
+            isInvalid={Boolean(errors.email)}
+            errorMessage={errors.email?.message}
+            {...register('email')}
+          />
+          <Controller
+            name='role'
+            control={control}
+            render={({ field }) => (
+              <Select
+                label='Cargo'
+                selectedKeys={new Set([field.value])}
+                onSelectionChange={(selected) => field.onChange(Array.from(selected)[0])}
+                isInvalid={Boolean(errors.role)}
+                errorMessage={errors.role?.message}
+              >
+                <SelectItem key='manager'>Gerente</SelectItem>
+                <SelectItem key='employee'>Funcionário</SelectItem>
+              </Select>
+            )}
+          />
+          <Input
+            label='Senha do funcionário'
+            isRequired
+            isInvalid={Boolean(errors.password)}
+            errorMessage={errors.password?.message}
+            {...register('password')}
+          />
+          <Input
+            label='Confirme a senha'
+            isRequired
+            isInvalid={Boolean(errors.confirmPassword)}
+            errorMessage={errors.confirmPassword?.message}
+            {...register('confirmPassword')}
+          />
+        </div>
+      </div>
+
+      <Divider className='my-2' />
+
+      <div className='flex items-center gap-3'>
+        <Button onClick={onCancel} isDisabled={isSubmiting}>
+          Cancelar
+        </Button>
+        <Button
+          type='submit'
+          color='primary'
+          isDisabled={!isDirty}
+          isLoading={isSubmiting}
+        >
+          Confirmar
+        </Button>
+      </div>
+    </form>
+  )
+}
