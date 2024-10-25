0c8fb688f1b39519dfa8f877b42b125e5c6435ea
0thigs <machinimavlogs@gmail.com>
2024-10-25 17:46:21 -0300

    ♻refactor: PrismaNotificationsRepository

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -3,13 +3,14 @@ import {
   PrismaInventoryMovementsRepository,
   PrismaProductsRepository,
   PrismaCategoriesRepository,
+  PrismaNotificationsRepository,
   PrismaCompaniesRepository,
-  PrismaUsersRepository,
 } from './prisma/repositories'
 
 export const productsRepository = new PrismaProductsRepository()
 export const batchesRepository = new PrismaBatchesRepository()
-export const usersRepository = new PrismaUsersRepository()
 export const inventoryMovementsRepository = new PrismaInventoryMovementsRepository()
 export const categoriesRepository = new PrismaCategoriesRepository()
+export const notificationsRepository = new PrismaNotificationsRepository()
 export const companiesRepository = new PrismaCompaniesRepository()
+
