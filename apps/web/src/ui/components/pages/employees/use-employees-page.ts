eb4c30dab12f01ff3d42a4c1ddd06ba6d4a5a840
kaufon <kauanfvale@gmail.com>
2024-10-25 16:49:13 -0300

    ♻️refactor: use employees page

--- apps/web/src/ui/components/pages/employees/use-employees-page.ts
+++ apps/web/src/ui/components/pages/employees/use-employees-page.ts
@@ -1,22 +1,51 @@
 import { useToast, useUrlParamNumber } from '@/ui/hooks'
-import { UsersFaker } from '@stocker/core/fakers'
+import type { UserDto } from '@stocker/core/dtos'
+import { useState } from 'react'
 
 export function useEmployeesPage() {
   const { showSuccess, showError } = useToast()
   const [page, setPage] = useUrlParamNumber('page', 1)
-
+  const [selectedEmployeesIds, setSelectdEmployeesIds] = useState<string[]>([])
   function handlePageChange(page: number) {
     setPage(page)
   }
   // THIS HERE GENERATES A ERROR SINCE ITS MOCK DATA IN CLIENT SIDE!!!! BUT ITS TEMPORARY SO WHATEVER
-  const tempoUser = UsersFaker.fakeMany()
+  const tempoUser: UserDto[] = [
+    {
+      id: '1',
+      role: 'manager',
+      email: 'manager@example.com',
+      name: 'Alice Manager',
+      password: 'securePassword1',
+    },
+    {
+      id: '2',
+      role: 'employee',
+      email: 'employee@example.com',
+      name: 'Bob Employee',
+      password: 'securePassword2',
+    },
+  ]
   const isLoading = false
   const totalPages = 11
+  async function handleUpdateEmployee() {
+    console.log('banana')
+  }
+  async function handleDeleteEmployeesAlertDialogConfirm() {
+    console.log(selectedEmployeesIds)
+  }
+  function handleEmployeesSelectionChange(employeesIds: string[]) {
+    setSelectdEmployeesIds(employeesIds)
+  }
   return {
+    handleDeleteEmployeesAlertDialogConfirm,
     totalPages: Math.ceil(totalPages / 10),
     page,
     handlePageChange,
     tempoUser,
     isLoading,
+    handleEmployeesSelectionChange,
+    selectedEmployeesIds,
+    handleUpdateEmployee,
   }
 }
