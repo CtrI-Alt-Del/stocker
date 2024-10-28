15ac42a5c1c2313549d8dbe00d7eb7be4d6cbe88
JohnPetros <joaopedro.nc@outlook.com>
2024-10-28 12:17:17 -0300

    ⚙️ config; add fastify handler

---
+++ apps/server/src/app/fastify/fastify-handler.ts
@@ -0,0 +1,17 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { FastifyHttp } from './fastify-http'
+
+import type { IController } from '@stocker/core/interfaces'
+
+export class FastifyHandler {
+  private readonly controller: IController
+
+  constructor(controller: IController) {
+    this.controller = controller
+  }
+
+  handle(request: FastifyRequest, reply: FastifyReply) {
+    const http = new FastifyHttp(request, reply)
+    return this.controller.handle(http)
+  }
+}
