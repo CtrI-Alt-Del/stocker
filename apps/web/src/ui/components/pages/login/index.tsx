163481900840f7aa7f8d9970e6d37842bd58eb87
JoaoGabrielGarcia <joaog.oliveiragarcia@gmail.com>
2024-10-28 19:34:09 -0300

    🚧 In Progress: Login and Register page responsive design

--- apps/web/src/ui/components/pages/login/index.tsx
+++ apps/web/src/ui/components/pages/login/index.tsx
@@ -7,7 +7,7 @@ export const LoginPage = () => {
   const { sm } = useBreakpoint()
   return (
     <div className='h-screen w-screen bg-orange flex justify-center items-center'>
-      <div className='bg-white    sm:w-1/2 w-full  rounded-xl grid grid-cols-1 md:grid-cols-2'>
+      <div className='bg-white    sm:w-1/2 md:w-3/4 w-full  rounded-xl grid grid-cols-1 md:grid-cols-2'>
         <div className='flex justify-center items-center mr-5 md:order-2 pt-12 sm:pt-0'>
           <Logo
             direction={sm ? 'flex-col' : 'flex-row'}
