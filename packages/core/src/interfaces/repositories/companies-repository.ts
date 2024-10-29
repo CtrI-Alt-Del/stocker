c69eaa17392b2836fe518d575f05ba5b98c3b9b8
FaelSantoss <silva.faelm@gmail.com>
2024-10-29 08:09:08 -0300

    🗄️ repositories: add method findByCnpj in CompaniesRepository

--- packages/core/src/interfaces/repositories/companies-repository.ts
+++ packages/core/src/interfaces/repositories/companies-repository.ts
@@ -3,6 +3,7 @@ import type { Company } from "../../domain/entities";
 export interface ICompaniesRepository {
 	add(company: Company): Promise<void>
 	findById(companyId: string): Promise<Company | null>
+	findByCnpj(companyId: string): Promise<Company | null>
 	update(company: Company, companyId: string): Promise<Company>
 	delete(companyId: string): Promise<void>
 }
