dbc12aca18fb8b7de5fb71bdb31eff7b0bb1f668
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-10-29 21:59:47 +0000

    🖥️ hooks: add auth service to use api hook

--- apps/web/src/api/services/auth-service.ts
+++ apps/web/src/api/services/auth-service.ts
@@ -4,11 +4,14 @@ import type { UserDto, CompanyDto } from '@stocker/core/dtos'
 export const AuthService = (apiClient: IApiClient): IAuthService => {
   return {
     async login(email: string, password: string) {
-      return await apiClient.post<{ jwt: string }>('auth/login', { email, password })
+      return await apiClient.post<{ jwt: string }>('/auth/login', { email, password })
     },
 
     async subscribe(userDto: UserDto, companyDto: CompanyDto) {
-      return await apiClient.post<{ jwt: string }>('auth/login', { userDto, companyDto })
+      return await apiClient.post<{ jwt: string }>('/auth/subscribe', {
+        user: userDto,
+        company: companyDto,
+      })
     },
   }
 }
