8d0740777c0c66a5b0aa72a9ef4e8ef95039bbdf
JohnPetros <joaopedro.nc@outlook.com>
2024-10-29 12:19:27 -0300

    [web] 📑 interfaces: implement auth service

---
+++ apps/web/src/api/services/auth-service.ts
@@ -0,0 +1,14 @@
+import type { IApiClient, IAuthService } from '@stocker/core/interfaces'
+import type { UserDto, CompanyDto } from '@stocker/core/dtos'
+
+export const AuthService = (apiClient: IApiClient): IAuthService => {
+  return {
+    async login(email: string, password: string) {
+      return await apiClient.post<{ jwt: string }>('auth/login', { email, password })
+    },
+
+    async subscribe(userDto: UserDto, companyDto: CompanyDto) {
+      return await apiClient.post<{ jwt: string }>('auth/login', { userDto, companyDto })
+    },
+  }
+}
