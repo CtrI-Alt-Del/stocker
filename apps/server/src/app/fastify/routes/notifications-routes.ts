9b4ed201ce66e8934c20899e7eb8145becf3d164
0thigs <machinimavlogs@gmail.com>
2024-10-30 12:57:24 -0300

    [back]🛣️routes: add list notifications routes

--- apps/server/src/app/fastify/routes/notifications-routes.ts
+++ apps/server/src/app/fastify/routes/notifications-routes.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from "fastify"
 import { FastifyHttp } from "../fastify-http"
-import { DeleteNotificationController } from "@/api/controllers/notifications"
+import { DeleteNotificationController, ListNotificationsController } from "@/api/controllers/notifications"
 
 export const NotificationsRoutes = async (app: FastifyInstance) => {
   const deleteNotificationController = new DeleteNotificationController()
+  const listNotificationsController = new ListNotificationsController()
+  
+  app.get('/', async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return listNotificationsController.handle(http)
+  })
   
   app.delete('/:notificationId', async (request, response) => {
     const http = new FastifyHttp(request, response)
