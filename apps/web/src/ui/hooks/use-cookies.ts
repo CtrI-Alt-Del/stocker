07f4cb5cd6a881593846fcd2bb5acb611a62aefe
JohnPetros <joaopedro.nc@outlook.com>
2024-10-30 10:19:43 -0300

    🖥 hooks: add use cookies

---
+++ apps/web/src/ui/hooks/use-cookies.ts
@@ -0,0 +1,28 @@
+import { cookies } from 'next/headers'
+
+export function useCookies() {
+  async function getCookie<Data>(key: string) {
+    'use server'
+
+    const cookie = cookies().get(key)
+    return cookie?.value ? (JSON.stringify(cookie.value) as Data) : null
+  }
+
+  async function setCookie(key: string, data: unknown) {
+    'use server'
+
+    cookies().set(key, JSON.stringify(data))
+  }
+
+  async function deleteCookie(key: string) {
+    'use server'
+
+    cookies().delete(key)
+  }
+
+  return {
+    getCookie,
+    setCookie,
+    deleteCookie,
+  }
+}
