cb8d5fc7f55dd9c4a04b0d4a395690d6014f9544
FaelSantoss <silva.faelm@gmail.com>
2024-10-30 12:56:25 -0300

    ✨ feat: create login route

--- apps/server/src/app/fastify/routes/auth-routes.ts
+++ apps/server/src/app/fastify/routes/auth-routes.ts
@@ -1,13 +1,19 @@
 import { DeleteCompanyController } from "@/api/controllers/companies";
 import type { FastifyInstance } from "fastify";
 import { FastifyHttp } from "../fastify-http";
-import { SubscribeController } from "@/api/controllers/auth";
+import { LoginController, SubscribeController } from "@/api/controllers/auth";
 
 export const AuthRoutes = async (app: FastifyInstance) => {
     const subscribeController = new SubscribeController()
+    const loginController = new LoginController()
 
     app.post('/subscribe', async (request, response) => {
         const http = new FastifyHttp(request, response)
         return subscribeController.handle(http)
+    })
+
+    app.post('/login', async (request, response) => {
+        const http = new FastifyHttp(request, response)
+        return loginController.handle(http)
     })
 }
