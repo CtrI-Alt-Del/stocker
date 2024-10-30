5fad5db055462d252d6f1b7c03a57bbc5ef210d3
kaufon <kauanfvale@gmail.com>
2024-10-30 12:11:06 -0300

    [back]🐛fix: fix users CRUD

--- apps/server/src/api/controllers/users/delete-users-controller.ts
+++ apps/server/src/api/controllers/users/delete-users-controller.ts
@@ -1,21 +1,18 @@
 import type { IHttp } from '@stocker/core/interfaces'
 import { DeleteUsersUseCase } from '@stocker/core/use-cases'
 
-import { usersRepository} from '@/database'
+import { usersRepository } from '@/database'
 
 type Body = {
   usersIds: string[]
-  companyId: string
 }
 
 export class DeleteUsersController {
   async handle(http: IHttp) {
-    const { usersIds,companyId } = http.getBody<Body>()
-
+    const { usersIds } = http.getBody<Body>()
     const useCase = new DeleteUsersUseCase(usersRepository)
     await useCase.execute({
       usersIds,
-      companyId
     })
     return http.send(null)
   }
