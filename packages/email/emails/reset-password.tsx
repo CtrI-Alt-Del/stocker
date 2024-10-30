0cbdc16c4cc955b041af1249ea6cb5b4e6204bef
kaufon <kauanfvale@gmail.com>
2024-10-30 18:06:21 -0300

    [front]🧱componenets: create reset password email template componente

---
+++ packages/email/emails/reset-password.tsx
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components'
+import * as React from 'react'
+
+type StockerResetPasswordEmailProps = {
+  baseUrl: string
+}
+
+export const StockerResetPasswordEmail = ({
+  baseUrl,
+}: StockerResetPasswordEmailProps) => (
+  <Tailwind
+    config={{
+      theme: {
+        extend: {
+          colors: {
+            brand: '#EF5C31',
+          },
+        },
+      },
+    }}
+  >
+    <Html className='bg-white font-sans'>
+      <Head />
+      <Preview>Redefinição de Senha - Stocker</Preview>
+      <Body className='bg-white font-sans'>
+        <Container className='mx-auto my-0 py-5 px-0'>
+          <Text className='text-lg leading-7 mt-2'>
+            Olá,
+            <br />
+            Recebemos um pedido para redefinir a senha da sua conta Stocker. Se você fez
+            essa solicitação, clique no botão abaixo para criar uma nova senha.
+          </Text>
+          <Section className='text-center mt-4'>
+            <Button
+              className='bg-brand rounded-lg text-white text-lg w-full py-3   inline-block'
+              href={`${baseUrl}`}
+            >
+              Redefinir Senha
+            </Button>
+          </Section>
+          <Text className='text-lg leading-7 mt-5'>
+            Caso não tenha solicitado a redefinição, recomendamos que você melhore sua
+            segurança e verifique suas informações.
+          </Text>
+          <Text className='text-lg leading-7 mt-5'>
+            Atenciosamente, <br />
+            <strong>Equipe Stocker</strong>
+          </Text>
+
+          <Hr className='border-gray-300 my-5' />
+          <Text className='text-xs text-gray-500'>
+            Parque de Inovação Tecnológica, São José dos Campos, São Paulo
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  </Tailwind>
+)
+
+StockerResetPasswordEmail.PreviewProps = {
+  baseUrl: 'http://localhost:3000',
+} as StockerResetPasswordEmailProps
+
+export default StockerResetPasswordEmail
