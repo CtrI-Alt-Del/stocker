4f1a776981326771e9350183d0af0e267ef997f0
JohnPetros <joaopedro.nc@outlook.com>
2024-10-31 09:06:04 -0300

    [back] 🐛 fix: jwt creation in login controller

--- apps/server/src/api/controllers/auth/login-controller.ts
+++ apps/server/src/api/controllers/auth/login-controller.ts
@@ -1,28 +1,23 @@
-import { usersRepository } from "@/database";
-import { cryptoProvider } from "@/providers";
-import type { IHttp } from "@stocker/core/interfaces";
-import { LoginUseCase } from "@stocker/core/use-cases";
-import type { UserDto } from "@stocker/core/dtos";
-import { HTTP_STATUS_CODE } from "@stocker/core/constants";
+import type { IHttp } from '@stocker/core/interfaces'
+import { LoginUseCase } from '@stocker/core/use-cases'
+import { HTTP_STATUS_CODE } from '@stocker/core/constants'
 
+import { usersRepository } from '@/database'
+import { cryptoProvider } from '@/providers'
 
 type Body = {
-    email: string,
-    password: string
+  email: string
+  password: string
 }
 
 export class LoginController {
-    async handle(http: IHttp) {
-      try {
-        const { email, password } = http.getBody<Body>()
-        const useCase = new LoginUseCase(usersRepository, cryptoProvider)
-        const userDto = await useCase.execute({email, password})
+  async handle(http: IHttp) {
+    const { email, password } = http.getBody<Body>()
+    const useCase = new LoginUseCase(usersRepository, cryptoProvider)
+    const userDto = await useCase.execute({ email, password })
 
-        const jwt = http.setJwt(userDto)
+    const jwt = await http.setJwt(userDto)
 
-        return http.send(jwt, HTTP_STATUS_CODE.ok)
-      } catch (error) {
-        return http.send(error, HTTP_STATUS_CODE.badRequest)
-      }
-    }
+    return http.send({ jwt }, HTTP_STATUS_CODE.ok)
   }
+}
