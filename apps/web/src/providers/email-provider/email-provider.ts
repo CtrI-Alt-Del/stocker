712fede57a50ade74c9b8644b7223a675cfb3304
kaufon <kauanfvale@gmail.com>
2024-10-31 18:32:27 -0300

    [front]🚧WIP:email provider

--- apps/web/src/providers/email-provider/email-provider.ts
+++ apps/web/src/providers/email-provider/email-provider.ts
@@ -1,10 +1,26 @@
+import { ResetPasswordTemplate } from '../../../../../packages/email/src/templates'
 import type { IEmailProvider } from '@stocker/core/interfaces'
-
+import { Resend } from 'resend'
+import { ApiError } from '@stocker/core/errors'
+import { BROWSER_ENV } from '@/constants'
 export class EmailProvider implements IEmailProvider {
-  sendPasswordResetEmail(
+  private resend: Resend
+  constructor() {
+    this.resend = new Resend(BROWSER_ENV.resendApiKey)
+  }
+  async sendPasswordResetEmail(
     recipientEmail: string,
     confirmationToken: string,
   ): Promise<void> {
-    throw new Error('Method not implemented.')
+      const {data,error} = await this.resend.emails.send({
+        from: `${BROWSER_ENV.senderName} <${BROWSER_ENV.senderEmail}>`,
+        to: [`${recipientEmail}`],
+        subject: 'Pedido de redefinição de senha',
+        react: ResetPasswordTemplate({ baseUrl: BROWSER_ENV.appUrl, token: confirmationToken }),
+      })
+      if (error) {
+      console.log(error)
+        
+      }
   }
 }
