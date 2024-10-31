91bba85e3841f5f4090721b1a559eca186372223
kaufon <kauanfvale@gmail.com>
2024-10-31 18:27:58 -0300

    [front]♻️refactor: add new variables to .env

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -6,16 +6,22 @@ import { stringSchema, urlSchema } from '@stocker/validation/schemas'
 const envSchema = z.object({
   appUrl: urlSchema,
   serverUrl: urlSchema,
-  passwordResetSecret: stringSchema,
+  resendApiKey: z.any(),
+  senderEmail: z.any(),
+  senderName: z.any()
 })
-
 const validation = envSchema.safeParse({
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
   serverUrl: process.env.NEXT_PUBLIC_SERVER_URL,
-  passwordResetSecret: process.env.PASSWORD_RESET_SECRET,
+  resendApiKey: process.env.RESEND_API_KEY,
+  senderName: process.env.SENDER_NAME,
+  senderEmail: process.env.SENDER_EMAIL,
 })
 
-if (!validation.success)
-  throw new AppError('Env Error', validation.error.issues.join(', '))
-
+if (!validation.success) {
+  const errorMessages = validation.error.issues
+    .map((issue) => `Path: ${issue.path.join('.')}, Message: ${issue.message}`)
+    .join('; ')
+  throw new AppError('Env Error', errorMessages)
+}
 export const ENV = validation.data
