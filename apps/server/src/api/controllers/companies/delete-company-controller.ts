cd17e46e823f756f3f4a900f143e94e562543ae2
JohnPetros <joaopedro.nc@outlook.com>
2024-10-31 16:39:36 -0300

    🎨 lint: format companies routes and delete company controller files

--- apps/server/src/api/controllers/companies/delete-company-controller.ts
+++ apps/server/src/api/controllers/companies/delete-company-controller.ts
@@ -1,20 +1,20 @@
-import type { IHttp } from "@stocker/core/interfaces";
+import type { IHttp } from '@stocker/core/interfaces'
 import { DeleteCompanyUseCase } from '@stocker/core/use-cases'
+import { HTTP_STATUS_CODE } from '@stocker/core/constants'
 
 import { companiesRepository } from '@/database'
-import { HTTP_STATUS_CODE } from "@stocker/core/constants";
 
 type RouteParams = {
-    companyId: string
+  companyId: string
 }
 
 export class DeleteCompanyController {
-    async handle(http: IHttp) {
-        const { companyId } = http.getRouteParams<RouteParams>()
-        const useCase = new DeleteCompanyUseCase(companiesRepository)
-        await useCase.execute({
-            companyId,
-        })
-        return http.send(null, HTTP_STATUS_CODE.ok)
-    }
+  async handle(http: IHttp) {
+    const { companyId } = http.getRouteParams<RouteParams>()
+    const useCase = new DeleteCompanyUseCase(companiesRepository)
+    await useCase.execute({
+      companyId,
+    })
+    return http.send(null, HTTP_STATUS_CODE.ok)
+  }
 }
