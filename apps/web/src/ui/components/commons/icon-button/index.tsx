ec23abd9bed93a716305a67ca7646f185b0ef955
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-10-31 22:43:40 +0000

    [front] 🧱 components: allor ref be passed into icon button

--- apps/web/src/ui/components/commons/icon-button/index.tsx
+++ apps/web/src/ui/components/commons/icon-button/index.tsx
@@ -1,3 +1,4 @@
+import { type ForwardedRef, forwardRef } from 'react'
 import { Button } from '@nextui-org/react'
 import { twMerge } from 'tailwind-merge'
 
@@ -13,23 +14,28 @@ type IconButton = {
   tag?: string
 }
 
-export const IconButton = ({ name, size, className, tag, onClick }: IconButton) => {
+const IconButtonComponent = (
+  { name, size, className, tag, onClick, ...buttonProps }: IconButton,
+  ref: ForwardedRef<HTMLButtonElement>,
+) => {
   return (
-    <>
-      <Button
-        isIconOnly
-        aria-label='OPA'
-        onClick={onClick}
-        className={twMerge('relative bg-transparent overflow-visible', className)}
-        size='sm'
-      >
-        <Icon name={name} size={size} />
-        {tag && (
-          <Tag type='danger' size='sm' className='absolute right-0 -top-3'>
-            {tag}
-          </Tag>
-        )}
-      </Button>
-    </>
+    <Button
+      ref={ref}
+      isIconOnly
+      aria-label='OPA'
+      onClick={onClick}
+      className={twMerge('relative bg-transparent overflow-visible', className)}
+      size='sm'
+      {...buttonProps}
+    >
+      <Icon name={name} size={size} />
+      {tag && (
+        <Tag type='danger' size='sm' className='absolute right-0 -top-3'>
+          {tag}
+        </Tag>
+      )}
+    </Button>
   )
 }
+
+export const IconButton = forwardRef(IconButtonComponent)
