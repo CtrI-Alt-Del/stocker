963261fa727a51b00ba2693c12b4f22400335dc6
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-10-31 21:01:00 +0000

    [front] ♻️ refactor: replace the old env var

--- apps/web/src/api/next/clients/next-server-api-client.ts
+++ apps/web/src/api/next/clients/next-server-api-client.ts
@@ -1,9 +1,9 @@
-import { ENV } from '@/constants'
+import { BROWSER_ENV } from '@/constants'
 import { NextApiClient } from './next-api-client'
 import type { CacheConfig } from '../types'
 
 export const NextServerApiClient = (cacheConfig?: CacheConfig) => {
   const apiClient = NextApiClient(cacheConfig)
-  apiClient.setBaseUrl(ENV.serverUrl)
+  apiClient.setBaseUrl(BROWSER_ENV.serverUrl)
   return apiClient
 }
