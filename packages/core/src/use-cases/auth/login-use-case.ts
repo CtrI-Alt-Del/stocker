18f97bb77be51256a28b586d7c1e46173737534e
JohnPetros <joaopedro.nc@outlook.com>
2024-10-31 09:07:05 -0300

    [core] ♻ refactor: use semantic errors in login use case

--- packages/core/src/use-cases/auth/login-use-case.ts
+++ packages/core/src/use-cases/auth/login-use-case.ts
@@ -1,31 +1,35 @@
-import type { ICryptoProvider, IUsersRepository } from "../../interfaces";
+import { NotAllowedError, NotFoundError } from '../../errors'
+import type { ICryptoProvider, IUsersRepository } from '../../interfaces'
 
 type Request = {
-    email: string,
-    password: string
-  }
+  email: string
+  password: string
+}
 
 export class LoginUseCase {
-    private readonly userRepository: IUsersRepository
-    private readonly cryptoProvider: ICryptoProvider
+  private readonly userRepository: IUsersRepository
+  private readonly cryptoProvider: ICryptoProvider
 
-    constructor(userRepository: IUsersRepository, cryptoProvider: ICryptoProvider) {
-        this.userRepository = userRepository
-        this.cryptoProvider = cryptoProvider
+  constructor(userRepository: IUsersRepository, cryptoProvider: ICryptoProvider) {
+    this.userRepository = userRepository
+    this.cryptoProvider = cryptoProvider
+  }
+
+  async execute({ email, password }: Request) {
+    const user = await this.userRepository.findByEmail(email)
+    if (!user) {
+      throw new NotFoundError('Usuário não encontrado')
     }
-      
-        async execute({ email, password }: Request) {
-      
-          const user = await this.userRepository.findByEmail(email);
-          if (!user) {
-            throw new Error('User not found');
-          }
-      
-          const isPasswordValid = await this.cryptoProvider.validateHash(password, user.password);
-          if (!isPasswordValid) {
-            throw new Error('Invalid credentials');
-          }
-    
-          return user.dto
-        }
-      }
+
+    const isPasswordValid = await this.cryptoProvider.validateHash(
+      password,
+      user.password,
+    )
+
+    if (!isPasswordValid) {
+      throw new NotAllowedError('Credenciais inválidas')
+    }
+
+    return user.dto
+  }
+}
