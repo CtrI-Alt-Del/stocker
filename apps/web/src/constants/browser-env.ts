91e863b71bdbae5beb9e79856b8f8405396750cd
kaufon <kauanfvale@gmail.com>
2024-10-31 18:31:56 -0300

    ♻️refactor: add new variables to brower_env constant

--- apps/web/src/constants/browser-env.ts
+++ apps/web/src/constants/browser-env.ts
@@ -6,11 +6,17 @@ import { urlSchema } from '@stocker/validation/schemas'
 const envSchema = z.object({
   appUrl: urlSchema,
   serverUrl: urlSchema,
+  resendApiKey: z.any(),
+  senderEmail: z.any(),
+  senderName: z.any(),
 })
 
 const validation = envSchema.safeParse({
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
   serverUrl: process.env.NEXT_PUBLIC_SERVER_URL,
+  resendApiKey: process.env.RESEND_API_KEY,
+  senderName: process.env.SENDER_NAME,
+  senderEmail: process.env.SENDER_EMAIL,
 })
 
 if (!validation.success) {
