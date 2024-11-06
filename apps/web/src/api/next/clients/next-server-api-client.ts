0ba3964e00e90daeaba70a0263d41522e063945f
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-11-06 20:49:44 +0000

    [front] 🐛 fix: getting jwt in server components

--- apps/web/src/api/next/clients/next-server-api-client.ts
+++ apps/web/src/api/next/clients/next-server-api-client.ts
@@ -1,9 +1,17 @@
-import { BROWSER_ENV } from '@/constants'
 import { NextApiClient } from './next-api-client'
-import type { CacheConfig } from '../types'
 
-export const NextServerApiClient = (cacheConfig?: CacheConfig) => {
+import { BROWSER_ENV, COOKIES } from '@/constants'
+import type { CacheConfig } from '../types'
+import { getCookieAction } from '@/actions'
+
+export const NextServerApiClient = async (cacheConfig?: CacheConfig) => {
   const apiClient = NextApiClient(cacheConfig)
+  const jwt = await getCookieAction(COOKIES.jwt.key)
+
+  if (jwt) {
+    apiClient.setHeader('Authorization', `Bearer ${jwt}`)
+  }
+
   apiClient.setBaseUrl(BROWSER_ENV.serverUrl)
   return apiClient
 }
--- apps/web/src/ui/components/pages/dashboard/summary-cards/index.tsx
+++ apps/web/src/ui/components/pages/dashboard/summary-cards/index.tsx
@@ -3,7 +3,7 @@ import { ReportsService } from '@/api/services'
 import { Card } from '@/ui/components/commons/card'
 
 export const SummaryCards = async () => {
-  const apiClient = NextServerApiClient({ isCacheEnabled: false })
+  const apiClient = await NextServerApiClient({ isCacheEnabled: false })
   const reportsService = ReportsService(apiClient)
   const data = await reportsService.reportSummary()
   const batchesCount = data.body.batchescount
@@ -12,28 +12,21 @@ export const SummaryCards = async () => {
   const outboundMovementsCount = data.body.outboundinventorymovementscount
 
   return (
-    <>
-      <div className='flex justify-center  items-center gap-6  flex-1 flex-col lg:flex-row   '>
-        <Card
-          title='Lotes'
-          value={batchesCount}
-          href='/records/products'
-          icon='package'
-        />
-        <Card title='Items' value={itemsCount} href='/inventory/stocks' icon='archive' />
-        <Card
-          title='Lançamentos de Entrada'
-          value={inboundMovementsCount}
-          href='/?movement_type=inbound'
-          icon='arrow-big-down-dash'
-        />
-        <Card
-          title='Lançamentos de Saída'
-          value={outboundMovementsCount}
-          href='/?movement_type=outbound'
-          icon='arrow-big-up-dash'
-        />
-      </div>
-    </>
+    <div className='flex justify-center  items-center gap-6  flex-1 flex-col lg:flex-row   '>
+      <Card title='Lotes' value={batchesCount} href='/records/products' icon='package' />
+      <Card title='Items' value={itemsCount} href='/inventory/stocks' icon='archive' />
+      <Card
+        title='Lançamentos de Entrada'
+        value={inboundMovementsCount}
+        href='/?movement_type=inbound'
+        icon='arrow-big-down-dash'
+      />
+      <Card
+        title='Lançamentos de Saída'
+        value={outboundMovementsCount}
+        href='/?movement_type=outbound'
+        icon='arrow-big-up-dash'
+      />
+    </div>
   )
 }
