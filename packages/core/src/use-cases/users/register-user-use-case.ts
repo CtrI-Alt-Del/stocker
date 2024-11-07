be4300d006dbe97baa5210a393ab657a04022c6b
JohnPetros <joaopedro.nc@outlook.com>
2024-11-07 18:33:45 -0300

    [core] ♻️ refactor: use queue provider on request password reset and register use

--- packages/core/src/use-cases/users/register-user-use-case.ts
+++ packages/core/src/use-cases/users/register-user-use-case.ts
@@ -1,20 +1,41 @@
-import { User } from "../../domain/entities";
-import type { UserDto } from "../../dtos";
-import type { IUsersRepository } from "../../interfaces/repositories/users-repository";
+import { User } from '../../domain/entities'
+import type { UserDto } from '../../dtos'
+import { NotFoundError } from '../../errors'
+import type {
+  ICompaniesRepository,
+  IQueueProvider,
+  IUsersRepository,
+} from '../../interfaces'
 
 type Request = {
   userDto: UserDto
 }
 export class RegisterUserUseCase {
   private readonly usersRepository: IUsersRepository
+  private readonly companiesRepository: ICompaniesRepository
+  private readonly queueProvider: IQueueProvider
 
-  constructor(usersRepository: IUsersRepository) {
+  constructor(
+    usersRepository: IUsersRepository,
+    companiesRepository: ICompaniesRepository,
+    queueProvider: IQueueProvider,
+  ) {
     this.usersRepository = usersRepository
+    this.companiesRepository = companiesRepository
+    this.queueProvider = queueProvider
   }
 
   async execute({ userDto }: Request) {
     const user = User.create(userDto)
     await this.usersRepository.add(user)
+    const company = await this.companiesRepository.findById(user.companyId)
+    if (!company) throw new NotFoundError('Empresa não encontrada')
+
+    this.queueProvider.push('send-welcome-employee-email', {
+      companyName: user.name,
+      employeeName: company.name,
+    })
+
     return user.id
   }
 }
