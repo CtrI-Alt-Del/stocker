128b5c6cbccc80e4de360e66cee84d2172f713bd
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-11-08 01:14:23 +0000

    🧱 components: render not found page if the user has no enough permission

---
+++ apps/web/src/app/(private)/not-found.tsx
@@ -0,0 +1,7 @@
+import { NotFoundPage } from '@/ui/components/pages/not-found'
+
+const Page = () => {
+  return <NotFoundPage />
+}
+
+export default Page
--- apps/web/src/app/(private)/records/employees/page.tsx
+++ apps/web/src/app/(private)/records/employees/page.tsx
@@ -1,6 +1,11 @@
+import { verifyUserRoleAction } from '@/actions'
 import { EmployeesPage } from '@/ui/components/pages/employees'
+import { notFound } from 'next/navigation'
 
-const Page = () => {
+const Page = async () => {
+  const isValidRole = await verifyUserRoleAction('admin')
+  if (!isValidRole) return notFound()
+
   return <EmployeesPage />
 }
 
