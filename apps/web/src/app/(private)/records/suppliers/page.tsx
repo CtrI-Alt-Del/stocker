ea4679fa8fd35b37bf59a2dfe0615f09d28ba95a
kaufon <kauanfvale@gmail.com>
2024-11-08 08:40:07 -0300

    [front]🔒auth: protect routes based on user role

--- apps/web/src/app/(private)/records/suppliers/page.tsx
+++ apps/web/src/app/(private)/records/suppliers/page.tsx
@@ -1,6 +1,13 @@
+import { verifyUserRoleAction } from '@/actions'
 import { SuppliersPage } from '@/ui/components/pages/suppliers'
+import { notFound } from 'next/navigation'
 
-const Page = () => {
+const Page = async () => {
+  const isValidRole = await verifyUserRoleAction('manager')
+  if (!isValidRole) {
+    return notFound()
+  }
+
   return <SuppliersPage />
 }
 
