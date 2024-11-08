528feed004cfabb97cdaf8eb1a26a06d08b8a8c4
kaufon <kauanfvale@gmail.com>
2024-11-08 16:15:19 -0300

    [back]🐛fix:fix reset password return

--- apps/server/src/api/controllers/auth/reset-password-controller.ts
+++ apps/server/src/api/controllers/auth/reset-password-controller.ts
@@ -1,8 +1,8 @@
-import { usersRepository } from "@/database";
-import { cryptoProvider } from "@/providers";
-import type { IHttp } from "@stocker/core/interfaces";
-import { ResetPasswordUseCase } from "@stocker/core/use-cases";
-import { HTTP_STATUS_CODE } from "@stocker/core/constants";
+import { usersRepository } from '@/database'
+import { cryptoProvider } from '@/providers'
+import type { IHttp } from '@stocker/core/interfaces'
+import { ResetPasswordUseCase } from '@stocker/core/use-cases'
+import { HTTP_STATUS_CODE } from '@stocker/core/constants'
 
 type Body = {
   email: string
@@ -12,8 +12,8 @@ export class ResetPasswordController {
   async handle(http: IHttp) {
     const { email, password } = http.getBody<Body>()
     const useCase = new ResetPasswordUseCase(usersRepository, cryptoProvider)
-
-    await useCase.execute({ email, password })
-    return http.send(null, HTTP_STATUS_CODE.ok)
+    const userDto = await useCase.execute({ email, password })
+    const jwt = await http.setJwt(userDto)
+    return http.send({ jwt }, HTTP_STATUS_CODE.ok)
   }
 }
