d082ba7153feb1261e6a1159d6ae04dad3833b38
0thigs <machinimavlogs@gmail.com>
2024-11-08 09:16:34 -0300

    [back]🧪test: fix register user use case test

--- packages/core/src/use-cases/users/tests/register-user-use-case.test.ts
+++ packages/core/src/use-cases/users/tests/register-user-use-case.test.ts
@@ -1,17 +1,37 @@
 import { describe, expect, it } from 'vitest'
 
-import { UsersRepositoryMock } from '../../../../__tests__/mocks/repositories'
-import { UsersFaker } from '../../../../__tests__/fakers'
+import {
+  UsersRepositoryMock,
+  CompanyRepositoryMock,
+} from '../../../../__tests__/mocks/repositories'
+import { UsersFaker, CompanyFaker } from '../../../../__tests__/fakers'
 import { RegisterUserUseCase } from '../register-user-use-case'
+import { CryptoProvider } from '../../../../../../apps/server/src/providers/crypto-provider'
+import { QueueProvider } from '../../../../../../apps/server/src/providers/queue-provider'
+import { Company } from '../../../domain/entities'
 
 describe('Register user use case', () => {
   it('should register a user', async () => {
     const userRepository = new UsersRepositoryMock()
-    const useCase = new RegisterUserUseCase(userRepository)
+    const companiesRepository = new CompanyRepositoryMock()
+    const cryptoProvider = new CryptoProvider()
+    const queueProvider = new QueueProvider({ jobs: [] })
+
+    const useCase = new RegisterUserUseCase(
+      userRepository,
+      companiesRepository,
+      cryptoProvider,
+      queueProvider,
+    )
 
     expect(userRepository.users).toHaveLength(0)
 
     const userDto = UsersFaker.fakeDto()
+
+    const companyDto = CompanyFaker.fakeDto({ id: userDto.companyId })
+    const company = Company.create(companyDto)
+    await companiesRepository.add(company)
+
     await useCase.execute({ userDto })
 
     expect(userRepository.users).toHaveLength(1)
