da3f5e891ea1650b5c2c5531618c47134177e686
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-11-11 00:47:27 +0000

    [back] 🛰️ middlewares: verify company on verify jwt

--- apps/server/src/api/middlewares/verify-jwt-middleware.ts
+++ apps/server/src/api/middlewares/verify-jwt-middleware.ts
@@ -1,3 +1,4 @@
+import { companiesRepository } from '@/database'
 import { NotAllowedError } from '@stocker/core/errors'
 import type { IHttp } from '@stocker/core/interfaces'
 
@@ -7,6 +8,13 @@ export class VerifyJwtMiddleware {
     if (!isValidJwt) {
       throw new NotAllowedError('Acesso não permitido')
     }
+
+    const user = await http.getUser()
+    const company = await companiesRepository.findById(user.companyId)
+    if (!company) {
+      throw new NotAllowedError('Empresa não encontrada')
+    }
+
     return http.pass()
   }
 }
