34f7ad76265882a038eeb150012b30d4903009a3
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-11-11 21:34:06 +0000

    [back] ✨ use cases: confirm auth use case

---
+++ packages/core/src/use-cases/auth/confirm-auth-use-case.ts
@@ -0,0 +1,31 @@
+import { NotFoundError } from '../../errors'
+import type { ICryptoProvider, IUsersRepository } from '../../interfaces'
+
+type Request = {
+  email: string
+  password: string
+}
+
+export class ConfirmAuthUseCase {
+  private readonly userRepository: IUsersRepository
+  private readonly cryptoProvider: ICryptoProvider
+
+  constructor(userRepository: IUsersRepository, cryptoProvider: ICryptoProvider) {
+    this.userRepository = userRepository
+    this.cryptoProvider = cryptoProvider
+  }
+
+  async execute({ email, password }: Request) {
+    const user = await this.userRepository.findByEmail(email)
+    if (!user) {
+      throw new NotFoundError('Usuário não encontrado')
+    }
+
+    const isPasswordValid = await this.cryptoProvider.validateHash(
+      password,
+      user.password,
+    )
+
+    return isPasswordValid
+  }
+}
