b70aa44d5cfcb35fdf407be8fe54519046f4b5c4
JohnPetros <joaopedro.nc@outlook.com>
2024-11-12 18:42:31 -0300

    [core] 📑 interfaces: add LocationsRepository

---
+++ packages/core/src/interfaces/repositories/locations-repository.ts
@@ -0,0 +1,11 @@
+import type { Location } from '../../domain/entities/location'
+import type { PaginationResponse } from '../../responses'
+import type { CategoriesListParams } from '../../types'
+
+export interface LocationsRepository {
+  findById(locationId: string): Promise<Location | null>
+  findMany(params: CategoriesListParams): Promise<PaginationResponse<Location>>
+  add(location: Location): Promise<void>
+  update(location: Location, locationId: string): Promise<void>
+  deleteMany(locationsIds: string[]): Promise<void>
+}
---
+++ packages/core/src/types/locations-list-params.ts
@@ -0,0 +1,5 @@
+export type LocationsListParams = {
+  name: string
+  page: number
+  companyId: string
+}
