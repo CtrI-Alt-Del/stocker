6fbd316e454a9decf6c0c4df1f6ed49bc9dd95fe
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-11-13 11:14:25 -0300

    [core]🗄️ repositories: add new function to find supplier by id

--- packages/core/src/interfaces/repositories/suppliers-repository.ts
+++ packages/core/src/interfaces/repositories/suppliers-repository.ts
@@ -2,11 +2,12 @@ import type { Supplier } from '../../domain/entities/supplier'
 import type { PaginationResponse } from '../../responses'
 import type { SuppliersListParams } from '../../types'
 
-export interface SuppliersRepository {
+export interface ISuppliersRepository {
   findMany(params: SuppliersListParams): Promise<PaginationResponse<Supplier>>
+  findById(supplierId: string): Promise<Supplier | null>
   findByEmail(email: string): Promise<Supplier>
-  findByCnpj(cnpj: string): Promise<Supplier>
-  findByPhone(phone: string): Promise<Supplier>
+  findByCnpj(cnpj: string | undefined): Promise<Supplier>
+  findByPhone(phone: string | undefined): Promise<Supplier>
   add(supplier: Supplier): Promise<void>
   update(supplier: Supplier, supplierId: string): Promise<void>
   deleteMany(suppliersIds: string[]): Promise<void>
