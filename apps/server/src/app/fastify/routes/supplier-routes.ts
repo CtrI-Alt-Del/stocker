76e21d8369372866626572fe1eb0930b598af3f0
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-11-13 11:12:53 -0300

    [back]🛣 routes: add update suppliers routes

--- apps/server/src/app/fastify/routes/supplier-routes.ts
+++ apps/server/src/app/fastify/routes/supplier-routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 
 import {
   RegisterSupplierController,
+  UpdateSupplierController,
 } from '@/api/controllers/suppliers'
 import { FastifyHttp } from '../fastify-http'
 import { FastifyHandler } from '../fastify-handler'
@@ -9,7 +10,8 @@ import { VerifyJwtMiddleware, VerifyUserRoleMiddleware } from '@/api/middlewares
 
 export const SuppliersRoutes = async (app: FastifyInstance) => {
   const registerSupplierController = new RegisterSupplierController()
-  const verifyJwtMiddleware = new FastifyHandler(new VerifyJwtMiddleware())
+  const updateSupplierController = new UpdateSupplierController()
+    const verifyJwtMiddleware = new FastifyHandler(new VerifyJwtMiddleware())
   const verifyAdminRoleMiddleware = new FastifyHandler(
     new VerifyUserRoleMiddleware('admin'),
   )
@@ -20,5 +22,10 @@ export const SuppliersRoutes = async (app: FastifyInstance) => {
   app.post('/', { preHandler: preHandlers }, async (request, response) => {
     const http = new FastifyHttp(request, response)
     return registerSupplierController.handle(http)
+  })
+
+  app.put('/:supplierId', { preHandler: preHandlers }, async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return updateSupplierController.handle(http)
   })
 }
