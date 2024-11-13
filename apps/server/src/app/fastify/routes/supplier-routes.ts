cad7ae1739dcf30b36ef8e23504cd7b2e5b8c9ad
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-11-12 22:55:19 -0300

    [back]🛣 routes: add register suppliers routes and endpoint

---
+++ apps/server/src/app/fastify/routes/supplier-routes.ts
@@ -0,0 +1,24 @@
+import type { FastifyInstance } from 'fastify'
+
+import {
+  RegisterSupplierController,
+} from '@/api/controllers/suppliers'
+import { FastifyHttp } from '../fastify-http'
+import { FastifyHandler } from '../fastify-handler'
+import { VerifyJwtMiddleware, VerifyUserRoleMiddleware } from '@/api/middlewares'
+
+export const SuppliersRoutes = async (app: FastifyInstance) => {
+  const registerSupplierController = new RegisterSupplierController()
+  const verifyJwtMiddleware = new FastifyHandler(new VerifyJwtMiddleware())
+  const verifyAdminRoleMiddleware = new FastifyHandler(
+    new VerifyUserRoleMiddleware('admin'),
+  )
+  const preHandlers = [verifyJwtMiddleware, verifyAdminRoleMiddleware].map((handler) =>
+    handler.handle.bind(handler),
+  )
+
+  app.post('/', { preHandler: preHandlers }, async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return registerSupplierController.handle(http)
+  })
+}
