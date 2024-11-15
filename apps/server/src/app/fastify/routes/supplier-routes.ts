1d9c53796645c7caf9d5a48cd905ed0cab8c07e7
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-11-15 20:50:26 -0300

    [back]🛣 routes: add delete suppliers routes

--- apps/server/src/app/fastify/routes/supplier-routes.ts
+++ apps/server/src/app/fastify/routes/supplier-routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 
 import {
   RegisterSupplierController,
+  DeleteSuppliersController,
 } from '@/api/controllers/suppliers'
 import { FastifyHttp } from '../fastify-http'
 import { FastifyHandler } from '../fastify-handler'
@@ -9,9 +10,10 @@ import { VerifyJwtMiddleware, VerifyUserRoleMiddleware } from '@/api/middlewares
 
 export const SuppliersRoutes = async (app: FastifyInstance) => {
   const registerSupplierController = new RegisterSupplierController()
+  const deleteSupplierController = new DeleteSuppliersController()
   const verifyJwtMiddleware = new FastifyHandler(new VerifyJwtMiddleware())
   const verifyAdminRoleMiddleware = new FastifyHandler(
-    new VerifyUserRoleMiddleware('admin'),
+    new VerifyUserRoleMiddleware('manager'),
   )
   const preHandlers = [verifyJwtMiddleware, verifyAdminRoleMiddleware].map((handler) =>
     handler.handle.bind(handler),
@@ -20,5 +22,10 @@ export const SuppliersRoutes = async (app: FastifyInstance) => {
   app.post('/', { preHandler: preHandlers }, async (request, response) => {
     const http = new FastifyHttp(request, response)
     return registerSupplierController.handle(http)
+  })
+
+  app.delete('/', { preHandler: preHandlers }, async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return deleteSupplierController.handle(http)
   })
 }
