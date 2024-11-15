40c7b9d4999c0c0d74b69570b59c7a89bd97a9ee
kaufon <kauanfvale@gmail.com>
2024-11-15 14:11:15 -0300

    [core]📑intefaces: add locations service

--- packages/core/src/dtos/index.ts
+++ packages/core/src/dtos/index.ts
@@ -1,4 +1,5 @@
 export type { ProductDto } from './product-dto'
+export type { LocationDto } from './location-dto'
 export type { BatchDto } from './batch-dto'
 export type { InventoryMovementDto } from './inventory-movement-dto'
 export type { UserDto } from './user-dto'
