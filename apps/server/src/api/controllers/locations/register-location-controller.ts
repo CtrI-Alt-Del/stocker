cb53ec21a42802ce3b7efd92fe267d74f8021d5f
FaelSantoss <silva.faelm@gmail.com>
2024-11-21 15:48:44 -0300

    [back] 🖥️ controllers: create register location controller

---
+++ apps/server/src/api/controllers/locations/register-location-controller.ts
@@ -0,0 +1,15 @@
+import type { LocationDto } from "@stocker/core/dtos";
+import type { IHttp } from "@stocker/core/interfaces";
+import { RegisterLocationUseCase } from '@stocker/core/use-cases'
+import { locationsRepository } from "@/database";
+import { HTTP_STATUS_CODE } from "@stocker/core/constants";
+
+export class RegisterLocationController {
+    async handle(http: IHttp) {
+        const locationDto = http.getBody<LocationDto>()
+        const useCase = new RegisterLocationUseCase(locationsRepository)
+        await useCase.execute({ locationDto })
+    
+        return http.send(null, HTTP_STATUS_CODE.created)
+    }
+}
