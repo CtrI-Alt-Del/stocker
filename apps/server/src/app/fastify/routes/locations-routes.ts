6f732cb183b9c31c9fbe809e1aae735a78cbee96
FaelSantoss <silva.faelm@gmail.com>
2024-11-22 13:43:33 -0300

    [back] ✨ feat: create route delete location

--- apps/server/src/app/fastify/routes/locations-routes.ts
+++ apps/server/src/app/fastify/routes/locations-routes.ts
@@ -1,12 +1,18 @@
-import { RegisterLocationController } from "@/api/controllers/locations";
+import { DeleteLocationsController, RegisterLocationController } from "@/api/controllers/locations";
 import type { FastifyInstance } from "fastify";
 import { FastifyHttp } from "../fastify-http";
 
 export const LocationsRoutes = async (app: FastifyInstance) => {
   const registerLocationController = new RegisterLocationController()
+  const deleteLocationsController = new DeleteLocationsController()
 
   app.post('/', async (request, response) => {
     const http = new FastifyHttp(request, response)
     return registerLocationController.handle(http)
+  })
+
+  app.delete('/', async (request, response) => {
+    const http = new FastifyHttp(request, response)
+    return deleteLocationsController.handle(http)
   })
 }
