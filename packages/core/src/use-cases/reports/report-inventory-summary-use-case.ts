eea7108489876df95dd7dcb670481cc901b654ed
0thigs <machinimavlogs@gmail.com>
2024-11-22 17:58:05 -0300

    [back]♻refactor: add inventory value to report inventory summary use case

--- packages/core/src/use-cases/reports/report-inventory-summary-use-case.ts
+++ packages/core/src/use-cases/reports/report-inventory-summary-use-case.ts
@@ -1,36 +1,43 @@
-import type { IBatchesRepository, IInventoryMovementsRepository } from '../../interfaces'
+import type { IBatchesRepository, IInventoryMovementsRepository, IProductsRepository } from '../../interfaces'
+
 type Request = {
   companyId:string
 }
 export class ReportInventorySummaryUseCase {
   private readonly batchesRepository: IBatchesRepository
   private readonly inventoryMovementsRepository: IInventoryMovementsRepository
+  private readonly productsRepository: IProductsRepository
   constructor(
     batchesRepository: IBatchesRepository,
     inventoryMovementsRepository: IInventoryMovementsRepository,
+    productsRepository: IProductsRepository,
   ) {
     this.batchesRepository = batchesRepository
     this.inventoryMovementsRepository = inventoryMovementsRepository
+    this.productsRepository = productsRepository
   }
 
   async execute({companyId}:Request) {
     const [
-      batchescount,
-      itemscount,
-      inboundinventorymovementscount,
-      outboundinventorymovementscount,
+      batchesCount,
+      itemsCount,
+      inboundInventoryMovementsCount,
+      outboundInventoryMovementsCount,
+      inventoryValue,
     ] = await Promise.all([
       this.batchesRepository.count(companyId),
       this.batchesRepository.countItems(companyId),
       this.inventoryMovementsRepository.countInbound(companyId),
       this.inventoryMovementsRepository.countOutbound(companyId),
+      this.productsRepository.calculateInventoryValue(companyId),
     ])
 
     return {
-      batchescount: batchescount,
-      itemscount: itemscount,
-      inboundinventorymovementscount: inboundinventorymovementscount,
-      outboundinventorymovementscount: outboundinventorymovementscount,
+      batchesCount,
+      itemsCount,
+      inboundInventoryMovementsCount,
+      outboundInventoryMovementsCount,
+      inventoryValue,
     }
   }
 }
