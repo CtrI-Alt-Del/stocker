5d8397682cc403f6cc1403f3142f11195b7df775
FaelSantoss <silva.faelm@gmail.com>
2024-11-28 07:42:42 -0300

    [core] ✨ feat: create update location use case

--- packages/core/src/use-cases/locations/index.ts
+++ packages/core/src/use-cases/locations/index.ts
@@ -1,2 +1,3 @@
 export { RegisterLocationUseCase } from './register-location-use-case'
 export { DeleteLocationsUseCase } from './delete-locations-use-case'
+export { UpdateLocationUseCase } from './update-location-use-case'
