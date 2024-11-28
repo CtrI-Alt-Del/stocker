2a001a91d74bf955d9f4681c7f09440cd7136a30
FaelSantoss <silva.faelm@gmail.com>
2024-11-28 07:44:35 -0300

    [back] ✨ feat: create update location route

--- apps/server/src/app/fastify/routes/locations-routes.ts
+++ apps/server/src/app/fastify/routes/locations-routes.ts
@@ -1,10 +1,12 @@
 import { DeleteLocationsController, RegisterLocationController } from "@/api/controllers/locations";
 import type { FastifyInstance } from "fastify";
 import { FastifyHttp } from "../fastify-http";
+import { UpdateLocationController } from "@/api/controllers/locations/update-location-controller";
 
 export const LocationsRoutes = async (app: FastifyInstance) => {
   const registerLocationController = new RegisterLocationController()
   const deleteLocationsController = new DeleteLocationsController()
+  const updateLocationController = new UpdateLocationController()
 
   app.post('/', async (request, response) => {
     const http = new FastifyHttp(request, response)
@@ -14,5 +16,10 @@ export const LocationsRoutes = async (app: FastifyInstance) => {
   app.delete('/', async (request, response) => {
     const http = new FastifyHttp(request, response)
     return deleteLocationsController.handle(http)
+  })
+
+  app.put('/:locationId', async (request, Response) => {
+    const http = new FastifyHttp(request, Response)
+    return updateLocationController.handle(http)
   })
 }
