7ad1550d4b8520fd0ed66472d770d7306f74b593
kaufon <kauanfvale@gmail.com>
2024-11-29 19:00:08 -0300

    [back]🐛fix: locations repository

--- packages/core/src/interfaces/repositories/locations-repository.ts
+++ packages/core/src/interfaces/repositories/locations-repository.ts
@@ -5,8 +5,10 @@ import type { CategoriesListParams } from '../../types'
 export interface ILocationsRepository {
   findById(locationId: string): Promise<Location | null>
   findByName(locationName: string): Promise<Location | null>
-  findMany(params: CategoriesListParams): Promise<PaginationResponse<Location>>
+  findMany(params: CategoriesListParams): Promise<Location[]>
+  count(): Promise<number>
   add(location: Location): Promise<void>
   update(location: Location, locationId: string): Promise<void>
   deleteMany(locationsIds: string[]): Promise<void>
+  deleteById(locationId: string): Promise<void>
 }
