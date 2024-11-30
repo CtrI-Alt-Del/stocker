5049801184802579684ff841062f7e1fe888b2ed
0thigs <machinimavlogs@gmail.com>
2024-11-29 23:20:43 -0300

    [back]🖥️controller: add name filter to controller

--- apps/server/src/api/controllers/locations/list-locations-controller.ts
+++ apps/server/src/api/controllers/locations/list-locations-controller.ts
@@ -1,21 +1,21 @@
+import { locationsRepository } from '@/database'
 import type { IHttp } from '@stocker/core/interfaces'
 import { ListLocationsUseCase } from '@stocker/core/use-cases'
 
-import { locationsRepository } from '@/database'
-import { HTTP_STATUS_CODE } from '@stocker/core/constants'
-
-type RouteParams = {
-  page: string
+type QueryParams = {
+  page: number
+  name?: string
 }
 
 export class ListLocationsController {
   async handle(http: IHttp) {
-    const { companyId } = await http.getUser()
-    const { page } = http.getQueryParams<RouteParams>()
-    const pageNumber = parseInt(page || '1', 10)
+    const user = await http.getUser()
+    const companyId = user.companyId
+    const { page, name } = http.getQueryParams<QueryParams>()
 
     const useCase = new ListLocationsUseCase(locationsRepository)
-    const response = await useCase.execute({ page: pageNumber, companyId: companyId })
-    return http.send(response, HTTP_STATUS_CODE.ok)
+    const locationsDto = await useCase.execute({ companyId, page, name })
+
+    return http.send(locationsDto)
   }
 }
