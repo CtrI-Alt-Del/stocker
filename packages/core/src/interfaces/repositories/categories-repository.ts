721c645a8c4eab4909a58018286dfd52c1b85a6f
0thigs <machinimavlogs@gmail.com>
2024-11-30 00:26:28 -0300

    [back]🐛fix: fix categories return

--- packages/core/src/interfaces/repositories/categories-repository.ts
+++ packages/core/src/interfaces/repositories/categories-repository.ts
@@ -3,7 +3,9 @@ import type { CategoriesListParams } from '../../types'
 
 export interface ICategoriesRepository {
   findById(categoryId: string): Promise<Category | null>
-  findMany(params: CategoriesListParams): Promise<Category[]>
+  findMany(
+    params: CategoriesListParams,
+  ): Promise<{ categories: Category[]; count: number }>
   add(category: Category): Promise<void>
   addMany(suppliers: Category[]): Promise<void>
   count(): Promise<number>
