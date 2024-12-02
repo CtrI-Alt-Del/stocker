67a543d8dd106bd2bd73dfaeb2bd3935c5255c7b
0thigs <machinimavlogs@gmail.com>
2024-12-02 00:50:29 -0300

    [front]✨feat: add favicon to the page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,18 +1,21 @@
-import type { Metadata } from 'next'
-import type { ReactNode } from 'react'
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
-import { RootLayout } from '@/ui/components/layouts/root'
-import '../ui/styles/globals.css'
+import { RootLayout } from "@/ui/components/layouts/root";
+import "../ui/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: 'Stocker',
-  description: 'Inventory management app',
-}
+  title: "Stocker",
+  description: "Inventory management app",
+  icons: {
+    icon: "https://i.imgur.com/ifXWlGH.png",
+  },
+};
 
 type LayoutProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export default function Layout({ children }: LayoutProps) {
-  return <RootLayout>{children}</RootLayout>
+  return <RootLayout>{children}</RootLayout>;
 }
